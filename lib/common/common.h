ca23cd031ff486f64e23d4dcd673c04956a9754d
crs <cschoeneman@gmail.com>
2004-11-09 18:31:25 +0000

    Added forgotten files.  Added define to common.h for OS X XCode build to get uname().

--- lib/common/common.h
+++ lib/common/common.h
@@ -57,6 +57,7 @@
 #		define HAVE_SYS_STAT_H 1
 #		define HAVE_SYS_TIME_H 1
 #		define HAVE_SYS_TYPES_H 1
+#		define HAVE_SYS_UTSNAME_H 1
 #		define HAVE_UNISTD_H 1
 #		define HAVE_VSNPRINTF 1
 #		define HAVE_WCHAR_H 1
