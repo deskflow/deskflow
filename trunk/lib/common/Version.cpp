141229e409bbe3d0ab307d1cbb1e4c98143b65c4
sorin.sbarnea <sorin.sbarnea@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2009-03-02 08:42:43 +0000

    Updated the version to 1.3.3, changed the named to Synergy+ and updated the project URL.

--- trunk/lib/common/Version.cpp
+++ trunk/lib/common/Version.cpp
@@ -1,11 +1,11 @@
 /*
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2004 Chris Schoeneman
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file COPYING that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -14,9 +14,9 @@
 
 #include "Version.h"
 
-const char* kApplication = "Synergy";
+const char* kApplication = "Synergy+";
 const char* kCopyright   = "Copyright (C) 2002 Chris Schoeneman";
 const char* kContact     = "Chris Schoeneman, crs23@bigfoot.com";
-const char* kWebsite     = "http://synergy2.sourceforge.net/";
-const char* kVersion	 = VERSION;
-const char* kAppVersion  = "Synergy " VERSION;
+const char* kWebsite     = "http://code.google.com/p/synergy-plus/";
+const char* kVersion   = VERSION;
+const char* kAppVersion  = "Synergy+ " VERSION;
