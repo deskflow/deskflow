27ff66e0753e258f06c52b4be2b2004d5cdb7317
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2010-10-06 15:02:15 +0000

    v2 doc - started on unit test diagram

--- doc/synergy2-spec/build.cmd
+++ doc/synergy2-spec/build.cmd
@@ -1,10 +1,12 @@
 @set name=synergy2-spec
 
-:: TODO: loop for all .mp files
-@mpost uml\server.mp --output-directory=uml
+:: process metapost files
+for /f %%f in ('dir /b uml\*.mp') do (
+    mpost uml\%%f --output-directory=uml
+)
 
-@pdflatex %name%
-@if not %ERRORLEVEL% equ 0 goto abort
+pdflatex %name%
+%if not %ERRORLEVEL% equ 0 goto abort
 
 :: run twice to resolve dependencies
 @if exist %name%.pdf pdflatex %name%
@@ -13,7 +15,7 @@
 :: then run the file if we actually generated stuff
 @if exist %name%.pdf %name%.pdf
 
-goto end
+@goto end
 
 :abort
 @echo
