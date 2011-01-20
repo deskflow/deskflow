54fdfdcea0e120203399c9689bba5128bc22697f
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2011-01-20 18:50:03 +0000

    backport fix for bug #143 - Cursor on Mac OS X goes to center when inactive

--- branches/1.4/lib/platform/COSXScreen.h
+++ branches/1.4/lib/platform/COSXScreen.h
@@ -33,6 +33,13 @@
 #include <IOKit/pwr_mgt/IOPMLib.h>
 #include <IOKit/IOMessage.h>
 
+extern "C" {
+    typedef int CGSConnectionID;
+    CGError CGSSetConnectionProperty(CGSConnectionID cid, CGSConnectionID targetCID, CFStringRef key, CFTypeRef value);
+    int _CGSDefaultConnection();
+}    
+
+
 template <class T>
 class CCondVar;
 class CEventQueueTimer;
@@ -112,6 +119,10 @@ private:
 
 	bool				onKey(CGEventRef event);
 	bool				onHotKey(EventRef event) const;
+	
+	// Added here to allow the carbon cursor hack to be called. 
+	void                showCursor();
+	void                hideCursor();
 
 	// map mac mouse button to synergy buttons
 	ButtonID			mapMacButtonToSynergy(UInt16) const;
@@ -167,6 +178,10 @@ private:
 										   CGEventType type,
 										   CGEventRef event,
 										   void* refcon);
+        static CGEventRef	handleCGInputEventSecondary(CGEventTapProxy proxy,
+                                                                   		   CGEventType type,
+                                                                   		   CGEventRef event,
+                                                                   		   void* refcon);
 private:
 	struct CHotKeyItem {
 	public:
