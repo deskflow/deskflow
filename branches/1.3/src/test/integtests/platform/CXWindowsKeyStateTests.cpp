9382bd558ec68e1a2fb7ad88fd0b4c8bc6a4bb53
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2011-06-02 22:53:05 +0000

    a few more x key state tests

--- branches/1.3/src/test/integtests/platform/CXWindowsKeyStateTests.cpp
+++ branches/1.3/src/test/integtests/platform/CXWindowsKeyStateTests.cpp
@@ -49,3 +49,64 @@ TEST_F(CXWindowsKeyStateTests, setActiveGroup_pollAndSet_groupIsZero)
 
 	ASSERT_EQ(0, keyState.getGroup());
 }
+
+TEST_F(CXWindowsKeyStateTests, setActiveGroup_poll_groupIsZero)
+{
+	CMockKeyMap keyMap;
+	CMockEventQueue eventQueue;
+	CXWindowsKeyState keyState(
+		m_display, true, (IEventQueue&)keyMap, (CKeyMap&)eventQueue);
+
+	keyState.setActiveGroup(CXWindowsKeyState::kGroupPoll);
+
+	ASSERT_EQ(-1, keyState.getGroup());
+}
+
+TEST_F(CXWindowsKeyStateTests, setActiveGroup_customGroup_groupWasSet)
+{
+	CMockKeyMap keyMap;
+	CMockEventQueue eventQueue;
+	CXWindowsKeyState keyState(
+		m_display, true, (IEventQueue&)keyMap, (CKeyMap&)eventQueue);
+
+	keyState.setActiveGroup(1);
+
+	ASSERT_EQ(1, keyState.getGroup());
+}
+
+TEST_F(CXWindowsKeyStateTests, mapModifiersFromX_zeroState_zeroMask)
+{
+	CMockKeyMap keyMap;
+	CMockEventQueue eventQueue;
+	CXWindowsKeyState keyState(
+		m_display, true, (IEventQueue&)keyMap, (CKeyMap&)eventQueue);
+
+	int mask = keyState.mapModifiersFromX(0);
+
+	ASSERT_EQ(0, mask);
+}
+
+TEST_F(CXWindowsKeyStateTests, mapModifiersToX_zeroMask_resultIsTrue)
+{
+	CMockKeyMap keyMap;
+	CMockEventQueue eventQueue;
+	CXWindowsKeyState keyState(
+		m_display, true, (IEventQueue&)keyMap, (CKeyMap&)eventQueue);
+
+	unsigned int modifiers = 0;
+	bool result = keyState.mapModifiersToX(0, modifiers);
+
+	ASSERT_TRUE(result);
+}
+
+TEST_F(CXWindowsKeyStateTests, fakeCtrlAltDel_default_returnsFalse)
+{
+	CMockKeyMap keyMap;
+	CMockEventQueue eventQueue;
+	CXWindowsKeyState keyState(
+		m_display, true, (IEventQueue&)keyMap, (CKeyMap&)eventQueue);
+
+	bool result = keyState.fakeCtrlAltDel();
+
+	ASSERT_FALSE(result);
+}
