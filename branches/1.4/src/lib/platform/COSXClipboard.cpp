85a249d28b5701a41245385e6fe6e0bc6ecae18c
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2011-10-22 13:13:09 +0000

    merged 1.3 r1125:1127 into 1.4

--- branches/1.4/src/lib/platform/COSXClipboard.cpp
+++ branches/1.4/src/lib/platform/COSXClipboard.cpp
@@ -15,8 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "CClipboard.h"
 #include "COSXClipboard.h"
+#include "CClipboard.h"
 #include "COSXClipboardUTF16Converter.h"
 #include "COSXClipboardTextConverter.h"
 #include "CLog.h"
