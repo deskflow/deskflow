9edbaf3474cb06fa2949b157fe5ae92f4dddf4aa
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2012-02-05 16:50:50 +0000

    improved unrecognised client log message

--- branches/1.4/src/lib/server/CServer.cpp
+++ branches/1.4/src/lib/server/CServer.cpp
@@ -303,7 +303,7 @@ CServer::adoptClient(CBaseClientProxy* client)
 
 	// name must be in our configuration
 	if (!m_config.isScreen(client->getName())) {
-		LOG((CLOG_WARN "a client with name \"%s\" is not in the map", client->getName().c_str()));
+		LOG((CLOG_WARN "unrecognised client name \"%s\", check config", client->getName().c_str()));
 		closeClient(client, kMsgEUnknown);
 		return;
 	}
