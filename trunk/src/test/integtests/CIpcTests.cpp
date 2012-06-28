5b01c8746a40dcacf98e266e66de203e1a7d4d4d
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2012-06-28 07:29:06 +0000

    skeleton for new ipc code.

---
+++ trunk/src/test/integtests/CIpcTests.cpp
@@ -0,0 +1,38 @@
+/*
+ * synergy -- mouse and keyboard sharing utility
+ * Copyright (C) 2012 Nick Bolton
+ * 
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file COPYING that should have accompanied this file.
+ * 
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+#include "CIpcServer.h"
+#include "CIpcClient.h"
+
+TEST(CIpcTests, serverSend)
+{
+	CIpcServer server;
+	server.listen();
+
+	CIpcClient client;
+	client.connect();
+}
+
+TEST(CIpcTests, clientSend)
+{
+	CIpcServer server;
+	server.listen();
+
+	CIpcClient client;
+	client.connect();
+}
