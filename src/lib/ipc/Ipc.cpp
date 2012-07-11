82d91605fac512a873b8d5a1a18f039875c725a4
Nick Bolton <nick@synergy-project.org>
2012-07-11 18:06:10 +0000

    changed ipc protocol to be more like original synergy protocol (4 byte message codes and better use of protocol util class).

--- src/lib/ipc/Ipc.cpp
+++ src/lib/ipc/Ipc.cpp
@@ -1,11 +1,11 @@
 /*
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2012 Nick Bolton
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file COPYING that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -15,25 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#include "Ipc.h"
 
-#define IPC_HOST "127.0.0.1"
-#define IPC_PORT 24801
-
-enum EIpcMessage {
-	kIpcHello,
-	kIpcLogLine,
-	kIpcCommand,
-	kIpcShutdown,
-};
-
-enum EIpcClientType {
-	kIpcClientUnknown,
-	kIpcClientGui,
-	kIpcClientNode,
-};
-
-extern const char*		kIpcMsgHello;
-extern const char*		kIpcMsgLogLine;
-extern const char*		kIpcMsgCommand;
-extern const char*		kIpcMsgShutdown;
+const char*				kIpcMsgHello		= "IHEL%1i";
+const char*				kIpcMsgLogLine		= "ILOG%s";
+const char*				kIpcMsgCommand		= "ICMD%s";
+const char*				kIpcMsgShutdown		= "ISDN";
