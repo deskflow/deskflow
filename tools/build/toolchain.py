1ca645602a5cc8d652e9103af267ded15122da72
Nick Bolton <nick@synergy-project.org>
2012-07-29 17:31:16 +0000

    reverted line breaks around get_generators

--- tools/build/toolchain.py
+++ tools/build/toolchain.py
@@ -956,17 +956,11 @@ class InternalCommands:
 	def get_generators(self):
 
 		if sys.platform == 'win32':
-
 			return self.win32_generators
-
 		elif sys.platform in ['linux2', 'sunos5', 'freebsd7', 'aix5']:
-
 			return self.unix_generators
-
 		elif sys.platform == 'darwin':
-
 			return self.darwin_generators
-
 		else:
 			raise Exception('Unsupported platform: ' + sys.platform)
 			
