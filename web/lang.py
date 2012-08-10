42c0cc25c0c91491436bcbf6fd27f3a59e45fa48
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2012-08-10 12:55:57 +0000

    web: script for importing translations

---
+++ web/lang.py
@@ -0,0 +1,41 @@
+import os, sys, shutil
+from getopt import gnu_getopt
+
+importSource = "synergy"
+importTarget = "public/locale"
+msgfmtFormat = "msgfmt -cv -o %s/website.mo %s/website.po"
+
+def runImport():
+	map = {
+		"cs-CZ" : "cs",
+		"hr-HR" : "hr",
+		"hu-HU" : "hu",
+		"ja-JP" : "ja",
+		"nl-NL" : "nl",
+		"pl-PL" : "pl",
+		"pt-PT" : "pt",
+		"pt-BR" : "pt_BR",
+		"sl-SI" : "sl",
+		"th-TH" : "th",
+		"tr-TR" : "tr",
+		"zh-CN" : "zh",
+		"zh-TW" : "zh_TW",
+	}
+
+	for file in os.listdir(importSource):
+		mapped = map[file] if file in map else file
+		source = "%s/%s/website_%s.po" % (importSource, file, file)
+		targetDir = "%s/%s/LC_MESSAGES" % (importTarget, mapped)
+		target = targetDir + "/website.po"
+		
+		print "copy '%s' to '%s'" % (source, target)
+		shutil.copy(source, target)
+		
+		cmd = (msgfmtFormat % (targetDir, targetDir))
+		print "run: " + cmd
+		os.system(cmd)
+
+opts, args = gnu_getopt(sys.argv, "i", "")
+for o, a in opts:
+	if o == "-i":
+		runImport()
