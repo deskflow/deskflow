681ff0fdce395a64f5c1693d46ffe19034f152bc
nick.bolton.uk <nick.bolton.uk@33c40d52-04ba-11de-a43f-ff96a2b7a9af>
2012-08-25 02:29:39 +0000

    web: updated db structure

--- web/synergy_web.sql
+++ web/synergy_web.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.7deb7
+-- version 3.4.10.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 05, 2012 at 06:51 PM
--- Server version: 5.1.61
--- PHP Version: 5.3.3-7+squeeze9
+-- Generation Time: Aug 25, 2012 at 02:29 AM
+-- Server version: 5.5.20
+-- PHP Version: 5.3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -22,17 +23,34 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `gwallet`
+--
+
+CREATE TABLE `gwallet` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `data` text NOT NULL,
+  `userId` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `paypal`
 --
 
-CREATE TABLE IF NOT EXISTS `paypal` (
+CREATE TABLE `paypal` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime NOT NULL,
   `email` varchar(250) NOT NULL,
   `ipnData` text NOT NULL,
   `ipnCheck` text NOT NULL,
+  `userId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `paypal` (
 -- Table structure for table `session`
 --
 
-CREATE TABLE IF NOT EXISTS `session` (
+CREATE TABLE `session` (
   `id` varchar(100) NOT NULL,
   `data` text NOT NULL,
   `expires` int(11) NOT NULL,
@@ -53,16 +71,19 @@ CREATE TABLE IF NOT EXISTS `session` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
   `password` varchar(32) NOT NULL,
   `created` datetime NOT NULL,
   `votesFree` int(11) NOT NULL,
+  `fundsTotal` decimal(10,2) NOT NULL,
+  `fundsCount` int(11) NOT NULL,
+  `fundsSignup` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -70,9 +91,13 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Table structure for table `vote`
 --
 
-CREATE TABLE IF NOT EXISTS `vote` (
+CREATE TABLE `vote` (
   `userId` int(11) NOT NULL,
   `issueId` int(11) NOT NULL,
   `voteCount` int(11) NOT NULL,
   UNIQUE KEY `id` (`issueId`,`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
