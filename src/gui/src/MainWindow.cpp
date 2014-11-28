581d4c54699447d90617d4a3942061eb7d7839c1
Xinyu Hou <xinyu@synergy-project.org>
2014-11-28 10:54:51 +0000

    Added a icon for download message box #4235

--- src/gui/src/MainWindow.cpp
+++ src/gui/src/MainWindow.cpp
@@ -1055,6 +1055,7 @@ void MainWindow::downloadBonjour()
 	if (m_DownloadMessageBox == NULL) {
 		m_DownloadMessageBox = new QMessageBox(this);
 		m_DownloadMessageBox->setWindowTitle("Synergy");
+		m_DownloadMessageBox->setIcon(QMessageBox::Information);
 		m_DownloadMessageBox->setText("Installing Bonjour, please wait...");
 		m_DownloadMessageBox->setStandardButtons(0);
 		m_pCancelButton = m_DownloadMessageBox->addButton(
