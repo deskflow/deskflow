74dda188dce4a19201fc8b45998967de6d771aea
Nick Bolton <nick@synergy-project.org>
2015-05-25 15:17:18 +0100

    Reintroduced accidentally removed functions #4695

--- ext/toolchain/commands1.py
+++ ext/toolchain/commands1.py
@@ -1439,14 +1439,37 @@ class InternalCommands:
 			self.getGitBranchName(), self.getGitRevision())
 		return re.sub(pattern, replace, self.dist_name(type))
 	
-	def getDebianArch(self):
-		if os.uname()[4][:3] == 'arm':
-			return 'armhf'
+  def getDebianArch(self):
+    if os.uname()[4][:3] == 'arm':
+      return 'armhf'
 
+                # os_bits should be loaded with '32bit' or '64bit'
+                import platform
+                (os_bits, other) = platform.architecture()
+    
+                # get platform based on current platform
+                if os_bits == '32bit':
+                        return 'i386'
+                elif os_bits == '64bit':
+                        return 'amd64'
+                else:
+                        raise Exception("unknown os bits: " + os_bits)
 
-		if os.uname()[4][:3] == 'arm':
-			return 'Linux-armv6l'
+        def getLinuxPlatform(self):
+    if os.uname()[4][:3] == 'arm':
+      return 'Linux-armv6l'
 
+                # os_bits should be loaded with '32bit' or '64bit'
+                import platform
+                (os_bits, other) = platform.architecture()
+    
+                # get platform based on current platform
+                if os_bits == '32bit':
+                        return 'Linux-i686'
+                elif os_bits == '64bit':
+                        return 'Linux-x86_64'
+                else:
+                        raise Exception("unknown os bits: " + os_bits)
 
 	def dist_usage(self):
 		print ('Usage: %s package [package-type]\n'
