5998b5c2defa9a7d741bd79c14158230b0080d88
Jerry (Xinyu Hou) <xinyu@synergy-project.org>
2015-07-09 14:29:23 -0700

    Removed redundant log #4858

--- src/lib/synergy/FileChunk.cpp
+++ src/lib/synergy/FileChunk.cpp
@@ -116,7 +116,6 @@ FileChunk::assemble(synergy::IStream* stream, String& dataReceived, size_t& expe
 	case kDataEnd:
 		if (expectedSize != dataReceived.size()) {
 			LOG((CLOG_ERR "corrupted clipboard data, expected size=%d actual size=%d", expectedSize, dataReceived.size()));
-			LOG((CLOG_DEBUG "file transmission failed: corrupted file data"));
 			return kError;
 		}
 
