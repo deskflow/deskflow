cbd63e9d67ead4199d5ab49b17d68a7583421d92
Jerry (Xinyu Hou) <xinyu@synergy-project.org>
2015-07-10 13:22:34 -0700

    Allowed reconnect after SSL fatal error #4857

--- src/lib/plugin/ns/SecureSocket.cpp
+++ src/lib/plugin/ns/SecureSocket.cpp
@@ -507,7 +507,6 @@ SecureSocket::getError()
 void
 SecureSocket::disconnect()
 {
-	sendEvent(getEvents()->forISocket().stopRetry());
 	sendEvent(getEvents()->forISocket().disconnected());
 	sendEvent(getEvents()->forIStream().inputShutdown());
 }
