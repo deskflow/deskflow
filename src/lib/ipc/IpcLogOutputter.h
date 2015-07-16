c3d38db053be85cc9fcc1d89dec8bbaeca729ecb
Jerry (Xinyu Hou) <xinyu@synergy-project.org>
2015-07-16 15:53:53 -0700

    Made buffer thread wait for notify when no gui #4721

--- src/lib/ipc/IpcLogOutputter.h
+++ src/lib/ipc/IpcLogOutputter.h
@@ -21,6 +21,7 @@
 #include "arch/Arch.h"
 #include "arch/IArchMultithread.h"
 #include "base/ILogOutputter.h"
+#include "ipc/Ipc.h"
 
 #include <deque>
 
@@ -39,7 +40,7 @@ public:
 	If \p useThread is \c false, then the buffer needs to be sent manually
 	using the \c sendBuffer() function.
 	*/
-	IpcLogOutputter(IpcServer& ipcServer, bool useThread);
+	IpcLogOutputter(IpcServer& ipcServer, EIpcClientType clientType, bool useThread);
 	virtual ~IpcLogOutputter();
 
 	// ILogOutputter overrides
@@ -92,6 +93,7 @@ private:
 	void				bufferThread(void*);
 	String				getChunk(size_t count);
 	void				appendBuffer(const String& text);
+	bool				isRunning();
 
 private:
 	typedef std::deque<String> Buffer;
@@ -113,4 +115,6 @@ private:
 	UInt16				m_bufferWriteCount;
 	double				m_bufferRateStart;
 	bool				m_useThread;
+	EIpcClientType		m_clientType;
+	ArchMutex			m_runningMutex;
 };
