77d3b18838615b2a526ef6127f7f6b628235477b
Scott Heavner <scott@olset.com>
2015-12-04 00:19:32 -0800

    OSX can be apple and makefile based.

--- ext/toolchain/commands1.py
+++ ext/toolchain/commands1.py
@@ -439,7 +439,7 @@ class InternalCommands:
 		elif generator.cmakeName.find('Unix Makefiles') != -1:
 			cmake_args += ' -DCMAKE_BUILD_TYPE=' + target.capitalize()
 			
-		elif sys.platform == "darwin":
+		if sys.platform == "darwin":
 			macSdkMatch = re.match("(\d+)\.(\d+)", self.macSdk)
 			if not macSdkMatch:
 				raise Exception("unknown osx version: " + self.macSdk)
