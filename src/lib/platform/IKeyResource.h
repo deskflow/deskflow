7f786cc8848bd7ccd3a48c5d76c56924d7f17a42
Jerry (Xinyu Hou) <xinyu@synergy-project.org>
2016-04-21 13:59:47 +0100

    Refactor KeyResource into own files #2765

--- src/lib/platform/IKeyResource.h
+++ src/lib/platform/IKeyResource.h
@@ -1,11 +1,11 @@
 /*
  * synergy -- mouse and keyboard sharing utility
- * Copyright (C) 2014 Synergy Si, Inc.
- *
+ * Copyright (C) 2016 Symless Ltd.
+ * 
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- *
+ * 
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -15,29 +15,22 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef COMMANDTHREAD_H
-#define COMMANDTHREAD_H
+#pragma once
 
-#include <QObject>
-#include <QStringList>
+#include "synergy/KeyState.h"
 
-class CommandProcess : public QObject
-{
-	Q_OBJECT
-
+class IKeyResource : public IInterface {
 public:
-	CommandProcess(QString cmd, QStringList arguments, QString input = "");
-
-signals:
-	void finished();
-
-public slots:
-	QString run();
-
-private:
-	QString m_Command;
-	QStringList m_Arguments;
-	QString m_Input;
+	virtual bool	isValid() const = 0;
+	virtual UInt32	getNumModifierCombinations() const = 0;
+	virtual UInt32	getNumTables() const = 0;
+	virtual UInt32	getNumButtons() const = 0;
+	virtual UInt32	getTableForModifier(UInt32 mask) const = 0;
+	virtual KeyID	getKey(UInt32 table, UInt32 button) const = 0;
+	
+	// Convert a character in the current script to the equivalent KeyID
+	static KeyID	getKeyID(UInt8);
+	
+	// Convert a unicode character to the equivalent KeyID.
+	static KeyID	unicharToKeyID(UniChar);
 };
-
-#endif // COMMANDTHREAD_H
