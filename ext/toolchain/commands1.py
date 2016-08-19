2030435405a69cf978b63c3f83b9a4305fb231a5
Leo Arias <yo@elopio.net>
2016-08-18 20:34:50 -0600

    Fix typo: s/paclage/package/

--- ext/toolchain/commands1.py
+++ ext/toolchain/commands1.py
@@ -1515,7 +1515,7 @@ class InternalCommands:
 			'Replace [package-type] with one of:\n'
 			'  src    .tar.gz source (Posix only)\n'
 			'  rpm    .rpm package (Red Hat)\n'
-			'  deb    .deb paclage (Debian)\n'
+			'  deb    .deb package (Debian)\n'
 			'  win    .exe installer (Windows)\n'
 			'  mac    .dmg package (Mac OS X)\n'
 			'\n'
