785d3c4f6a304788c8a728855fada4f04c833579
Andrew Nelless <andrew@symless.com>
2016-09-07 15:24:00 +0100

    Update company copyright notice

--- src/lib/synergy/ArgParser.cpp
+++ src/lib/synergy/ArgParser.cpp
@@ -1,6 +1,6 @@
 /*
  * synergy -- mouse and keyboard sharing utility
- * Copyright (C) 2014 Synergy Si, inc.
+ * Copyright (C) 2014-2016 Symless Ltd.
  * 
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
