660625d79130c004804d8f7868693fab5e136052
Andrew Nelless <andrew@symless.com>
2016-09-13 10:35:53 +0100

    Restore safe reinterpret_casts in MSWindowsDesks

--- src/lib/platform/MSWindowsDesks.cpp
+++ src/lib/platform/MSWindowsDesks.cpp
@@ -241,7 +241,7 @@ void
 MSWindowsDesks::getCursorPos(SInt32& x, SInt32& y) const
 {
 	POINT pos;
-	sendMessage(SYNERGY_MSG_CURSOR_POS, static_cast<WPARAM>(&pos), 0);
+	sendMessage(SYNERGY_MSG_CURSOR_POS, reinterpret_cast<WPARAM>(&pos), 0);
 	x = pos.x;
 	y = pos.y;
 }
@@ -427,7 +427,7 @@ void
 MSWindowsDesks::destroyClass(ATOM windowClass) const
 {
 	if (windowClass != 0) {
-		UnregisterClass(static_cast<LPCTSTR>(windowClass),
+		UnregisterClass(reinterpret_cast<LPCTSTR>(windowClass),
 							MSWindowsScreen::getWindowInstance());
 	}
 }
@@ -437,7 +437,7 @@ MSWindowsDesks::createWindow(ATOM windowClass, const char* name) const
 {
 	HWND window = CreateWindowEx(WS_EX_TRANSPARENT |
 									WS_EX_TOOLWINDOW,
-								static_cast<LPCTSTR>(windowClass),
+								reinterpret_cast<LPCTSTR>(windowClass),
 								name,
 								WS_POPUP,
 								0, 0, 1, 1,
@@ -757,7 +757,7 @@ MSWindowsDesks::deskThread(void* vdesk)
 			break;
 
 		case SYNERGY_MSG_CURSOR_POS: {
-			POINT* pos = static_cast<POINT*>(msg.wParam);
+			POINT* pos = reinterpret_cast<POINT*>(msg.wParam);
 			if (!GetCursorPos(pos)) {
 				pos->x = m_xCenter;
 				pos->y = m_yCenter;
