33ebe61ef21d13fea7a6c6c9858ad2303d595540
Jerry (Xinyu Hou) <xinyu@synergy-project.org>
2016-10-14 12:43:01 +0100

    #5657 Counted expiring within 1 day as 1 day left

--- src/lib/shared/SerialKey.cpp
+++ src/lib/shared/SerialKey.cpp
@@ -101,13 +101,16 @@ unsigned long long
 SerialKey::dayLeft(unsigned long long currentTime) const
 {
 	unsigned long long timeLeft =  0;
+	unsigned long long day = 60 * 60 * 24;
+	
 	if (m_expireTime > currentTime) {
 		timeLeft = m_expireTime - currentTime;
 	}
 
-	unsigned long long day = 60 * 60 * 24;
+	unsigned long long dayLeft = 0;
+	dayLeft = timeLeft % day != 0 ? 1 : 0;
 	
-	return timeLeft / day;
+	return timeLeft / day + dayLeft;
 }
 
 std::string
