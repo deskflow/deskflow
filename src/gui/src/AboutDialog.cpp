7cd4642e92ad9c1a136dd93ecd6c5427f294218f
Andrew Nelless <andrew@symless.com>
2016-12-19 17:40:24 +0000

    Remove ugly Synergy version from about dialog

--- src/gui/src/AboutDialog.cpp
+++ src/gui/src/AboutDialog.cpp
@@ -29,7 +29,6 @@ AboutDialog::AboutDialog(QWidget* parent, const QString& synergyApp) :
 
 	m_versionChecker.setApp(synergyApp);
 	QString version = m_versionChecker.getVersion();
-	version = version + '-' + VERSION_STAGE +  '-' + VERSION_REVISION;
 	m_pLabelSynergyVersion->setText(version);
 
 	QString buildDateString = QString::fromLocal8Bit(__DATE__).simplified();
