0058ae9e8006e5afe363723139f5507750140fe4
Andrew Nelless <andrew@symless.com>
2017-05-26 22:10:15 +0100

    Update OpenSSL to v1.0.2l

--- ext/openssl/windows/x64/include/openssl/opensslconf.h
+++ ext/openssl/windows/x64/include/openssl/opensslconf.h
@@ -118,8 +118,8 @@ extern "C" {
 
 #if !(defined(VMS) || defined(__VMS)) /* VMS uses logical names instead */
 #if defined(HEADER_CRYPTLIB_H) && !defined(OPENSSLDIR)
-#define ENGINESDIR "C:\\Users\\Synergy\\Desktop\\openssl-w64/lib/engines"
-#define OPENSSLDIR "C:\\Users\\Synergy\\Desktop\\openssl-w64/ssl"
+#define ENGINESDIR "C:\\OpenSSL/lib/engines"
+#define OPENSSLDIR "C:\\OpenSSL/ssl"
 #endif
 #endif
 
