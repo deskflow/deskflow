3b20310deba118275a239f26336b73449bd93fa6
walker0643 <>
2018-02-16 11:55:16 -0500

    update macos bundle icon

--- res/makeicon.sh
+++ res/makeicon.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ICNS_BASE=../dist/macos/bundle/Barrier.app/Contents/Resources
 if ! which magick >/dev/null 2>&1; then
     echo "Need ImageMagic for this"
     exit 10
@@ -10,9 +11,12 @@ if [ ! -r barrier.png ]; then
 fi
 rm -rf work || exit $?
 mkdir -p work || exit $?
-for s in 16 24 32 48 64 128; do
-    magick convert barrier.png -resize ${s}x${s} work/${s}.png || exit $?
+for s in 16 24 32 48 64 128 256 512 1024; do
+    magick convert barrier.png -resize ${s}x${s} -depth 8 work/${s}.png || exit $?
 done
+# windows icon
 magick convert work/{16,24,32,48,64,128}.png barrier.png barrier.ico || exit $?
+# macos icon
+png2icns $ICNS_BASE/Barrier.icns work/{16,32,256,512,1024}.png || exit $?
 rm -rf work
 echo Done
