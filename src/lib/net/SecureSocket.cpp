b994c94a908f1d3b57f93ef2517bf5c2a9e820c4
walker0643 <>
2018-02-24 18:20:14 -0500

    fix build for libressl. thanks to truatpasteurdotfr for the patch

--- src/lib/net/SecureSocket.cpp
+++ src/lib/net/SecureSocket.cpp
@@ -814,7 +814,7 @@ SecureSocket::showSecureCipherInfo()
         showCipherStackDesc(sStack);
     }
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 	// m_ssl->m_ssl->session->ciphers is not forward compatable,
 	// In future release of OpenSSL, it's not visible,
     STACK_OF(SSL_CIPHER) * cStack = m_ssl->m_ssl->session->ciphers;
