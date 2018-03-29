c5e70af09a4cab3212056bbf98020705bbe4c544
walker0643 <>
2018-03-29 15:50:23 -0400

    DataDirectories header should be shared between platform-specific implementations

--- src/lib/common/DataDirectories.h
+++ src/lib/common/DataDirectories.h
@@ -1,7 +1,21 @@
 #pragma once
 
-#ifdef _WIN32
-#include "win32/DataDirectories.h"
-#else
-#include "unix/DataDirectories.h"
-#endif
+#include <string>
+
+class DataDirectories
+{
+public:
+    static const std::string& personal();
+    static const std::string& personal(const std::string& path);
+
+    static const std::string& profile();
+    static const std::string& profile(const std::string& path);
+
+private:
+    // static class
+    DataDirectories() {}
+
+    static std::string _personal;
+    static std::string _profile;
+};
+
