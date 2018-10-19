b8fa610f8f294bbe75b60cfb16ce53284f395977
Povilas Kanapickas <povilas@radix.lt>
2018-10-19 20:17:04 +0300

    Build full installer via clean_build.sh (#157)
    * OSX: Fix lack of newlines in build messages
    
    * OSX: Build full installer as part of the installer build via cmake

--- osx_environment.sh
+++ osx_environment.sh
@@ -2,13 +2,13 @@
 
 if [ ! $BARRIER_BUILD_ENV ]; then
 
-    printf "Modifying environment for Barrier build..."
+    printf "Modifying environment for Barrier build...\n"
 
     if command -v port; then
-        printf "Detected Macports"
+        printf "Detected Macports\n"
 
         if [ ! -d /opt/local/lib/cmake/Qt5 ]; then
-            printf "Please install qt5-qtbase port"
+            printf "Please install qt5-qtbase port\n"
         fi
         export BARRIER_BUILD_MACPORTS=1
         export CMAKE_PREFIX_PATH="/opt/local/lib/cmake/Qt5:$CMAKE_PREFIX_PATH"
@@ -17,7 +17,7 @@ if [ ! $BARRIER_BUILD_ENV ]; then
         export PKG_CONFIG_PATH="/opt/local/libexec/qt5/lib/pkgconfig:$PKG_CONFIG_PATH"
 
     elif command -v brew; then
-        printf "Detected Homebrew"
+        printf "Detected Homebrew\n"
         QT_PATH=$(brew --prefix qt)
         OPENSSL_PATH=$(brew --prefix openssl)
 
@@ -28,7 +28,7 @@ if [ ! $BARRIER_BUILD_ENV ]; then
         export PKG_CONFIG_PATH="$OPENSSL_PATH/lib/pkgconfig:$PKG_CONFIG_PATH"
 
     else
-        printf "Neither Homebrew nor Macports is installed. Can't get dependency paths"
+        printf "Neither Homebrew nor Macports is installed. Can't get dependency paths\n"
         exit 1
     fi
 
