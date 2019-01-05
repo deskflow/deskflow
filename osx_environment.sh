b6861a7d3c5dd3a7c78699df7f96138be5131d87
Girts Folkmanis <girtsf@users.noreply.github.com>
2019-01-05 11:19:28 -0800

    add checks to osx_environment.sh
    Check if Xcode and required packages are installed, otherwise fail
    early.

--- osx_environment.sh
+++ osx_environment.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# Checks if directory exists, otherwise asks to install package.
+function check_dir_exists() {
+    local path=$1
+    local package=$2
+
+    if [ ! -d "$path" ]; then
+        echo "Please install $package"
+        exit 1
+    fi
+}
+
 if [ ! $BARRIER_BUILD_ENV ]; then
+    check_dir_exists '/Applications/Xcode.app' 'Xcode'
 
     printf "Modifying environment for Barrier build...\n"
 
     if command -v port; then
         printf "Detected Macports\n"
 
-        if [ ! -d /opt/local/lib/cmake/Qt5 ]; then
-            printf "Please install qt5-qtbase port\n"
-        fi
+        check_dir_exists '/opt/local/lib/cmake/Qt5' 'qt5-qtbase port'
+
         export BARRIER_BUILD_MACPORTS=1
         export CMAKE_PREFIX_PATH="/opt/local/lib/cmake/Qt5:$CMAKE_PREFIX_PATH"
         export LD_LIBRARY_PATH="/opt/local/lib:$LD_LIBRARY_PATH"
@@ -20,6 +31,9 @@ if [ ! $BARRIER_BUILD_ENV ]; then
         printf "Detected Homebrew\n"
         QT_PATH=$(brew --prefix qt)
         OPENSSL_PATH=$(brew --prefix openssl)
+
+        check_dir_exists "$QT_PATH" 'qt'
+        check_dir_exists "$OPENSSL_PATH" 'openssl'
 
         export BARRIER_BUILD_BREW=1
         export CMAKE_PREFIX_PATH="$QT_PATH:$CMAKE_PREFIX_PATH"
