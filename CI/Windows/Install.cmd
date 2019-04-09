c83cb89b780eed6931ef3dd2b647c99f5d09524f
Jamie Newbon <Jamie@symless.com>
2019-04-09 12:08:27 +0100

    Testing windows build

--- CI/Windows/Install.cmd
+++ CI/Windows/Install.cmd
@@ -3,14 +3,10 @@ start /wait msiexec.exe /i C:\TEMP\node-install.msi /l*vx "%TEMP%\MSI-node-insta
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v Disabled /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v DontShowUI /t REG_DWORD /d 1 /f
 
-C:\TEMP\InstallVS.cmd C:\TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache `
-    --channelUri C:\TEMP\VisualStudio.chman `
-    --installChannelUri C:\TEMP\VisualStudio.chman `
-    --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended`
-    --installPath C:\BuildTools
+C:\TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache --channelUri C:\TEMP\VisualStudio.chman --installChannelUri C:\TEMP\VisualStudio.chman --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --installPath C:\BuildTools
 
-powershell -command "$Env:QT_INSTALL_DIR = 'C:\\Qt'; Start-Process C:\qt.exe -ArgumentList '--verbose --script C:\qtifwsilent.qs' -NoNewWindow -Wait"
+powershell -command "$Env:QT_INSTALL_DIR = 'C:\\Qt'; Start-Process C:\TEMP\qt.exe -ArgumentList '--verbose --script C:\TEMP\qtifwsilent.qs' -NoNewWindow -Wait"
 
-del C:\TEMP\node-install.msi
+del C:\TEMP\qt.exe
 del C:\TEMP\vs_buildtools.exe
-del C:\qt.exe
+del C:\TEMP\node-install.msi
