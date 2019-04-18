18ac0fe4b5ff17d3009067b4389a6fa284b7323a
noisyshape <noisyshape@gmail.com>
2019-04-18 18:39:29 -0400

    Replace Wix with Inno Setup
    The Wix configuration is left over from Synergy.

--- build_installer.bat
+++ build_installer.bat
@@ -1,19 +1,18 @@
 @echo off
-set WIX_ROOT=C:\Program Files (x86)\WiX Toolset v3.11
+set INNO_ROOT=C:\Program Files (x86)\Inno Setup 5
 
 set savedir=%cd%
 cd /d %~dp0
 
 if not exist build\bin\Release goto buildproject
 
-cd build\installer-wix
-if ERRORLEVEL 1 goto buildproject
-
 echo Building 64-bit Windows installer...
-"%WIX_ROOT%\bin\candle.exe" -nologo -arch x64 -dConfiguration=Release -dPlatform=x64 -ext WixUtilExtension -ext WixFirewallExtension Product.wxs -o Barrier.wixobj
+
+cd build\installer-inno
+if ERRORLEVEL 1 goto buildproject
+"%INNO_ROOT%\ISCC.exe" /Qp barrier.iss
 if ERRORLEVEL 1 goto failed
-"%WIX_ROOT%\bin\light.exe" -nologo -ext WixUtilExtension -ext WixFirewallExtension -ext WixUIExtension Barrier.wixobj -o bin\Barrier.msi
-if ERRORLEVEL 1 goto failed
+
 echo Build completed successfully
 goto done
 
@@ -29,7 +28,7 @@ goto done
 echo Build failed
 
 :done
-set WIX_ROOT=
+set INNO_ROOT=
 
 cd /d %savedir%
 set savedir=
