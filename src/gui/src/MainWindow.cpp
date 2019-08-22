26d50d1e184eeaa2e030b4624ea8b4d2a2f21788
Jamie Newbon <48688400+Jnewbon@users.noreply.github.com>
2019-08-22 10:10:46 +0100

    #6541 Fixed connected info text to include without TLS encryption

--- src/gui/src/MainWindow.cpp
+++ src/gui/src/MainWindow.cpp
@@ -975,7 +975,7 @@ void MainWindow::setSynergyState(qSynergyState state)
             setStatus(tr("Synergy is connected (with %1)").arg(m_SecureSocketVersion));
         }
         else {
-            setStatus(tr("Synergy is running (without %1)").arg(m_SecureSocketVersion));
+            setStatus(tr("Synergy is running (without TLS encryption)").arg(m_SecureSocketVersion));
         }
         break;
     }
