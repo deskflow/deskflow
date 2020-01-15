f8595fd33edac8ae5835be736183d29376625cc5
Jamie Newbon <Jamie@symless.com>
2020-01-15 10:25:55 +0000

    Updated Version and changelog

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -6,8 +6,8 @@ cmake_minimum_required (VERSION 3.4)
 
 set (SYNERGY_VERSION_MAJOR 1)
 set (SYNERGY_VERSION_MINOR 11)
-set (SYNERGY_VERSION_PATCH 0)
-set (SYNERGY_VERSION_STAGE "stable")
+set (SYNERGY_VERSION_PATCH 1)
+set (SYNERGY_VERSION_STAGE "snapshot")
 
 #
 # Version from CI
