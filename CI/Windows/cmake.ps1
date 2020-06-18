eb0b99432315e16140127cc754ad454cee3e3fe3
Jamie Newbon <jamie@symless.com>
2020-06-18 10:52:16 +0100

    #6674 Used -eq

--- CI/Windows/cmake.ps1
+++ CI/Windows/cmake.ps1
@@ -2,7 +2,7 @@ call "%ENV_BAT%" -arch=x%ARCH%
 set BONJOUR_SDK_HOME=%cd%\%BONJOUR_SDK_DIR%
 mkdir build%ARCH%
 cd build%ARCH%
-IF ("%Enterprise%"=="true") {
+IF ("%Enterprise%" -eq "true") {
     cmake -G "Visual Studio 16 2019" -A %MSARCH% -V -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_PATH% -DSYNERGY_ENTERPRISE=ON ..
 } else {
     cmake -G "Visual Studio 16 2019" -A %MSARCH% -V -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_PATH% ..
