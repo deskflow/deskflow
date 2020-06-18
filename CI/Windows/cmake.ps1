6c89efed42f82c8e30fa389743e3bdc12539069b
Jamie Newbon <jamie@symless.com>
2020-06-18 10:49:50 +0100

    #6674 Corrected if syntax for powershell

--- CI/Windows/cmake.ps1
+++ CI/Windows/cmake.ps1
@@ -2,8 +2,8 @@ call "%ENV_BAT%" -arch=x%ARCH%
 set BONJOUR_SDK_HOME=%cd%\%BONJOUR_SDK_DIR%
 mkdir build%ARCH%
 cd build%ARCH%
-IF "%Enterprise%"=="true" (
+IF ("%Enterprise%"=="true") {
     cmake -G "Visual Studio 16 2019" -A %MSARCH% -V -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_PATH% -DSYNERGY_ENTERPRISE=ON ..
-) else (
+} else {
     cmake -G "Visual Studio 16 2019" -A %MSARCH% -V -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_PATH% ..
-)
+}
