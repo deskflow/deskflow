b3682d40ef1cf3faeca8ed5696e3104e730a35d8
Ignacio Rodriguez <ignacionr@gmail.com>
2021-01-10 01:53:07 +0700

    expanded test on Config

--- src/test/unittests/server/ConfigTests.cpp
+++ src/test/unittests/server/ConfigTests.cpp
@@ -18,6 +18,20 @@
 #include "lib/server/Config.h"
 #include "test/global/gtest.h"
 
+class OnlySystemFilter: public InputFilter::Condition {
+    public:
+        Condition*        clone() const override {
+            return new OnlySystemFilter();
+        }
+        String            format() const override {
+            return "";
+        }
+
+        InputFilter::EFilterStatus    match(const Event& ev) override {
+            return ev.getType() == Event::kSystem ? InputFilter::kActivate : InputFilter::kNoMatch;
+        }
+};
+
 TEST(ServerConfigTests, serverconfig_will_deem_inequal_configs_with_different_map_size)
 {
     Config a(nullptr);
@@ -41,8 +55,27 @@ TEST(ServerConfigTests, serverconfig_will_deem_equal_configs_with_same_cell_name
 {
     Config a(nullptr);
     Config b(nullptr);
-    a.addScreen("screenA");
-    b.addScreen("screenA");
+    EXPECT_TRUE(a.addScreen("screenA"));
+    EXPECT_TRUE(a.addScreen("screenB"));
+    EXPECT_TRUE(a.addScreen("screenC"));
+    EXPECT_TRUE(a.connect("screenA", EDirection::kBottom, 0.0f, 0.5f, "screenB", 0.5f, 1.0f));
+    EXPECT_TRUE(a.connect("screenB", EDirection::kLeft, 0.0f, 0.5f, "screenB", 0.5f, 1.0f));
+    EXPECT_TRUE(b.addScreen("screenA"));
+    EXPECT_TRUE(b.addScreen("screenB"));
+    EXPECT_TRUE(b.addScreen("screenC"));
+    EXPECT_TRUE(b.connect("screenA", EDirection::kBottom, 0.0f, 0.5f, "screenB", 0.5f, 1.0f));
+    EXPECT_TRUE(b.connect("screenB", EDirection::kLeft, 0.0f, 0.5f, "screenB", 0.5f, 1.0f));
+    a.addOption("screenA", kOptionClipboardSharing, 1);
+    b.addOption("screenA", kOptionClipboardSharing, 1);
+    a.addOption(std::string(), kOptionClipboardSharing, 1);
+    b.addOption(std::string(), kOptionClipboardSharing, 1);
+    a.getInputFilter()->addFilterRule(InputFilter::Rule{new OnlySystemFilter()});
+    b.getInputFilter()->addFilterRule(InputFilter::Rule{new OnlySystemFilter()});
+    a.addAlias("screenA", "aliasA");
+    b.addAlias("screenA", "aliasA");
+    a.setSynergyAddress(NetworkAddress(8080));
+    b.setSynergyAddress(NetworkAddress(8080));
+
     EXPECT_TRUE(a == b);
     EXPECT_TRUE(b == a);
 }
@@ -93,20 +126,6 @@ TEST(ServerConfigTests, serverconfig_will_deem_different_configs_with_different_
     EXPECT_FALSE(a == b);
     EXPECT_FALSE(b == a);
 }
-
-class OnlySystemFilter: public InputFilter::Condition {
-    public:
-        Condition*        clone() const override {
-            return new OnlySystemFilter();
-        }
-        String            format() const override {
-            return "";
-        }
-
-        InputFilter::EFilterStatus    match(const Event& ev) override {
-            return ev.getType() == Event::kSystem ? InputFilter::kActivate : InputFilter::kNoMatch;
-        }
-};
 
 TEST(ServerConfigTests, serverconfig_will_deem_different_configs_with_different_filters)
 {
