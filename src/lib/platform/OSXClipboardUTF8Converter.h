7128f8f5f01aa75b82f4b34262416041105a0c3e
SerhiiGadzhilov <71632867+SerhiiGadzhilov@users.noreply.github.com>
2021-01-27 21:38:52 +0200

    Fixed #6839 Wrong text encoding in iMessage (#6927)
    * Fixed #6839 Wrong text encoding in iMessage
    
    * Update ChangeLog

---
+++ src/lib/platform/OSXClipboardUTF8Converter.h
@@ -0,0 +1,30 @@
+/*
+ * synergy -- mouse and keyboard sharing utility
+ * Copyright (C) 2012-2016 Symless Ltd.
+ * Copyright (C) 2004 Chris Schoeneman
+ *
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file LICENSE that should have accompanied this file.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "platform/OSXClipboardAnyTextConverter.h"
+
+class OSXClipboardUTF8Converter : public OSXClipboardAnyTextConverter
+{
+public:
+    virtual CFStringRef getOSXFormat() const;
+private:
+    virtual String doFromIClipboard(const String&) const;
+    virtual String doToIClipboard(const String&) const;
+};
+
