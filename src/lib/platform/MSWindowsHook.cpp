826554a957e4f801cb568d2276c8fc7fc0568298
Andrii Batyiev <andrii-external@symless.com>
2021-04-29 01:26:25 +0300

    SYNERGY-907 Client tray language switcher reverts to EN *Dummy commit to update test coverage statistic

--- src/lib/platform/MSWindowsHook.cpp
+++ src/lib/platform/MSWindowsHook.cpp
@@ -159,6 +159,7 @@ static
 void
 keyboardGetState(BYTE keys[256], DWORD vkCode, bool kf_up)
 {
+    auto test = 24;
     // we have to use GetAsyncKeyState() rather than GetKeyState() because
     // we don't pass through most keys so the event synchronous state
     // doesn't get updated.  we do that because certain modifier keys have
