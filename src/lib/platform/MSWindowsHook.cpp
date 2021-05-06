2360fa16af41146d67d01ba3824f22a6f18310bd
Andrii Batyiev <andrii-external@symless.com>
2021-05-06 09:11:44 +0300

    SYNERGY-907 Client tray language switcher reverts to EN *Remove debug code

--- src/lib/platform/MSWindowsHook.cpp
+++ src/lib/platform/MSWindowsHook.cpp
@@ -159,7 +159,6 @@ static
 void
 keyboardGetState(BYTE keys[256], DWORD vkCode, bool kf_up)
 {
-    LOG((CLOG_ERR "test output for code covarage generation %s", std::to_string(vkCode).c_str()));
     // we have to use GetAsyncKeyState() rather than GetKeyState() because
     // we don't pass through most keys so the event synchronous state
     // doesn't get updated.  we do that because certain modifier keys have
