27e3b4414c4c7f3ade8a4dddad237ae9ae5dda70
Andrii Batyiev <andrii-external@symless.com>
2021-05-19 03:32:32 +0300

    SYNERGY-960 Test IPv6 support on various Operating systems *Temp compilation hotfix

--- src/gui/src/ScreenList.h
+++ src/gui/src/ScreenList.h
@@ -25,7 +25,7 @@ class ScreenList : public QList<Screen>
     int m_width = 5;
 
 public:
-    explicit ScreenList(int width = 5);
+    explicit ScreenList(int width = 5) {};
 
     /**
      * @brief addScreenByPriority adds a new screen according to the following priority:
@@ -40,7 +40,7 @@ public:
      * 9.In case all places from the list have already booked, place in any spare place
      * @param newScreen
      */
-    void addScreenByPriority(const Screen& newScreen);
+    void addScreenByPriority(const Screen& newScreen) {};
 
     /**
      * @brief addScreenToFirstEmpty adds screen into the first empty place
