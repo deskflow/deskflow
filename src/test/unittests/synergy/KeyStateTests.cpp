34f481d733b2504577f76778d192f8eee37dff09
Andrii Batyiev <andrii-external@symless.com>
2021-09-21 18:54:18 +0300

    SYNERGY-907 Client tray language switcher reverts to EN *Fix build

--- src/test/unittests/synergy/KeyStateTests.cpp
+++ src/test/unittests/synergy/KeyStateTests.cpp
@@ -494,7 +494,7 @@ assertMaskIsOne(ForeachKeyCallback cb, void* userData)
 
 const synergy::KeyMap::KeyItem*
 stubMapKey(synergy::KeyMap::Keystrokes& keys, KeyID, SInt32,
-           const synergy::KeyMap::ModifierToKeys&, const KeyModifierMask&,
+           synergy::KeyMap::ModifierToKeys&, KeyModifierMask&,
            KeyModifierMask, bool, const String&)
 {
     keys.push_back(s_stubKeystroke);
