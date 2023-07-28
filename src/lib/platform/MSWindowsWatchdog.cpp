cd02bedc0501a5c774b9d29074e53fa00bdc39d5
Nick Bolton <nick@symless.com>
2023-07-28 01:08:08 +0100

    Only show advanced proc info at DEBUG2

--- src/lib/platform/MSWindowsWatchdog.cpp
+++ src/lib/platform/MSWindowsWatchdog.cpp
@@ -387,7 +387,8 @@ MSWindowsWatchdog::startProcess()
         m_processRunning = true;
         m_processFailures = 0;
 
-        LOG((CLOG_DEBUG "started process, session=%i, elevated: %s, command=%s",
+        LOG((CLOG_DEBUG "started core process from daemon");
+        LOG((CLOG_DEBUG2 "process info, session=%i, elevated: %s, command=%s",
             m_session.getActiveSessionId(),
             m_elevateProcess ? "yes" : "no",
             m_command.c_str()));
