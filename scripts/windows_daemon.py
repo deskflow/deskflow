3ee9cad4602a30c2eb22e0c1a3d14e4cbdc08029
Nick Bolton <nick@symless.com>
2024-01-16 13:45:15 +0000

    Add more args for flexibility

--- scripts/windows_daemon.py
+++ scripts/windows_daemon.py
@@ -5,35 +5,39 @@ import sys
 import argparse
 import glob
 
-BIN_FILE_BASE = 'synergyd'
+BIN_NAME = 'synergyd'
 SOURCE_BIN_DIR = os.path.join('build', 'bin')
-SOURCE_BIN_GLOB = f'{BIN_FILE_BASE}*'
 TARGET_BIN_DIR = 'bin'
-TARGET_BIN_FILE = f'{os.path.join(TARGET_BIN_DIR, BIN_FILE_BASE)}.exe'
 SERVICE_NOT_RUNNING_ERROR = 2
 
 def main():
   """Entry point for the script."""
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('-p', action='store_true')
+  parser.add_argument('--pause-on-exit', action='store_true')
+  parser.add_argument('--source-bin-dir', default=SOURCE_BIN_DIR)
+  parser.add_argument('--target-bin-dir', default=TARGET_BIN_DIR)
+  parser.add_argument('--source-bin-name', default=BIN_NAME)
+  parser.add_argument('--target-bin-name', default=BIN_NAME)
   args = parser.parse_args()
-
-  try:
-    reinstall()
-  except Exception as e:
-    print(f'Error: {e}')
-  
-  if (args.p):
-    input('Press any key to continue...')
-
-def reinstall():
-  """Stops the running daemon service, copies files, and reinstalls."""
 
   if not is_admin():
     print('Re-launching script as admin')
-    ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, f'{__file__} -p', None, 1)
+    args = ' '.join(sys.argv[1:])
+    command = f'{__file__} --pause-on-exit {args}'
+    ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, command, None, 1)
     sys.exit()
+
+  try:
+    reinstall(args.source_bin_dir, args.target_bin_dir, args.source_bin_name, args.target_bin_name)
+  except Exception as e:
+    print(f'Error: {e}')
+  
+  if (args.pause_on_exit):
+    input('Press any key to continue...')
+
+def reinstall(source_bin_dir, target_bin_dir, source_bin_name, target_bin_name):
+  """Stops the running daemon service, copies files, and reinstalls."""
   
   print('Stopping daemon service')
   try:
@@ -44,24 +48,40 @@ def reinstall():
     else:
       raise e
 
-  print(f'Persisting dir: {TARGET_BIN_DIR}')
-  os.makedirs(TARGET_BIN_DIR, exist_ok=True)
+  copy_bin_files(source_bin_dir, target_bin_dir, source_bin_name, target_bin_name)
 
-  source_files = glob.glob(os.path.join(SOURCE_BIN_DIR, SOURCE_BIN_GLOB))
+  target_bin_file = f'{os.path.join(target_bin_dir, target_bin_name)}.exe'
+
+  print('Removing old daemon service')
+  subprocess.run([target_bin_file, '/uninstall'], shell=True, check=True)
+
+  print('Installing daemon service')
+  subprocess.run([target_bin_file, '/install'], shell=True, check=True)
+
+def copy_bin_files(source_bin_dir, target_bin_dir, source_bin_name, target_bin_name):
+
+  if not os.path.isdir(source_bin_dir):
+    raise Exception(f'Invalid source bin dir: {source_bin_dir}')
+
+  print(f'Persisting dir: {target_bin_dir}')
+  os.makedirs(target_bin_dir, exist_ok=True)
+
+  source_bin_glob = f'{source_bin_name}*'
+  source_files = glob.glob(os.path.join(source_bin_dir, source_bin_glob))
+
+  if not source_files:
+    raise Exception(f'No files found in {source_bin_dir} matching {source_bin_glob}')
+
   for source_file in source_files:
-    target_file = os.path.join(TARGET_BIN_DIR, os.path.basename(source_file))
+    base_name = os.path.basename(source_file)
+    base_name = base_name.replace(source_bin_name, target_bin_name)
+    target_file = os.path.join(target_bin_dir, base_name)
     print(f'Copying {source_file} to {target_file}')
-    # use the copy command; shutil.copy gives us a permission denied error.
+  # use the copy command; shutil.copy gives us a permission denied error.
     try:
       subprocess.run(['copy', source_file, target_file], shell=True, check=True)
     except subprocess.CalledProcessError as e:
       print(f'Copy failed: {e}')
-  
-  print('Removing old daemon service')
-  subprocess.run([TARGET_BIN_FILE, '/uninstall'], shell=True, check=True)
-
-  print('Installing daemon service')
-  subprocess.run([TARGET_BIN_FILE, '/install'], shell=True, check=True)
 
 def is_admin():
     """Returns True if the current process has admin privileges."""
