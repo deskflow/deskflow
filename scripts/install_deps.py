d0fcdc6b98a51bfbee3f6bdd298cf78a20bffa23
Nick Bolton <nick@symless.com>
2024-06-11 13:46:54 +0100

    YAML file to describe Linux deps

--- scripts/install_deps.py
+++ scripts/install_deps.py
@@ -3,6 +3,7 @@ from lib import windows
 import subprocess
 import sys
 import argparse
+import yaml
 
 def main():
   """Entry point for the script."""
@@ -29,12 +30,11 @@ class Deps:
       self.windows()
     elif (sys.platform == 'darwin'):
       self.mac()
+    elif (sys.platform.startswith('linux')):
+      self.linux()
     else:
       print(f'Unsupported platform: {sys.platform}')
-  
-  def mac(self):
-    """Installs dependencies on macOS."""
-    subprocess.run('brew bundle --file=Brewfile', shell=True, check=True)
+      sys.exit(1)
 
   def windows(self):
     """Installs dependencies on Windows."""
@@ -50,6 +50,49 @@ class Deps:
     
     self.choco('Chocolatey.config', ci_env)
 
+  def mac(self):
+    """Installs dependencies on macOS."""
+    self.run('brew bundle --file=Brewfile')
+
+  def linux(self):
+    """Installs dependencies on Linux."""
+    distro = self.linux_distro()
+    if not distro:
+      print("Unable to detect Linux distro")
+      sys.exit(1)
+
+    file = 'linux-packages.yml'
+    with open(file, 'r') as f:
+      data = yaml.safe_load(f)
+
+    root = data.get('linux-packages', {})
+    distro_data = root.get(distro, [])
+    if not distro_data:
+      print(f'Nothing found in {file} for: {distro}')
+      sys.exit(1)
+
+    command = distro_data['command']
+    package_data = distro_data['packages']
+
+    if not command:
+      print(f'No package command found in {file} for: {distro}')
+      sys.exit(1)
+
+    if not package_data:
+      print(f'No package list found in {file} for: {distro}')
+      sys.exit(1)
+    
+    package_list = ' '.join(package_data)
+    self.run(f'{command} {package_list}')
+    
+  def linux_distro(self):
+    if os.path.isfile('/etc/os-release'):
+        with open('/etc/os-release') as f:
+            for line in f:
+                if line.startswith('ID='):
+                    return line.strip().split('=')[1].strip('"')
+    return None
+
   def choco(self, config, ci_env):
     """Installs packages using Chocolatey."""
     
@@ -62,7 +105,7 @@ class Deps:
     # auto-accept all prompts
     args.extend(['-y'])
 
-    subprocess.run(args, shell=True, check=True)
+    self.run(args)
 
   def choco_ci(self):
     """Configures Chocolatey cache for CI."""
@@ -73,8 +116,13 @@ class Deps:
       # sets the choco cache dir, which should match the dir in the ci cache action.
       key_arg = '--name="cacheLocation"'
       value_arg = f'--value="{runner_temp}/choco"'
-      subprocess.run(['choco', 'config', 'set', key_arg, value_arg], shell=True, check=True)
+      self.run(['choco', 'config', 'set', key_arg, value_arg])
     else:
       print(f'Warning: CI environment variable {runner_temp_key} not set')
+
+  def run(self, args):
+    """Runs a command."""
+    print(f'Running: {args}')
+    subprocess.run(args, shell=True, check=True)
 
 main()
