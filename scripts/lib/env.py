cb415c197f020c4963fb3773dfa6b57dd42268f7
Nick Bolton <nick@symless.com>
2024-06-20 19:57:22 +0100

    Update package managers

--- scripts/lib/env.py
+++ scripts/lib/env.py
@@ -30,8 +30,10 @@ def install_dependencies():
 
     distro = get_linux_distro()
     if distro == "ubuntu" or distro == "debian":
+        cmd_utils.run(f"{sudo} apt update", check=False)
         cmd_utils.run(f"{sudo} apt install -y python3-pip python3-venv")
     elif distro == "fedora" or distro == "centos":
+        cmd_utils.run(f"{sudo} dnf check-update", check=False)
         cmd_utils.run(f"{sudo} dnf install -y python3-pip python3-venv")
     else:
         raise RuntimeError(f"Failed to install Python dependencies on {distro}")
