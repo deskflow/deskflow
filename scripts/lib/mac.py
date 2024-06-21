b0cd02a75ace435ce212c512438ecc5d2387d060
Nick Bolton <nick@symless.com>
2024-06-21 16:05:40 +0100

    Add codesigning certificate validation for Mac signing

--- scripts/lib/mac.py
+++ scripts/lib/mac.py
@@ -59,6 +59,12 @@ def package(config):
     )
 
     codesign_id = env.get_env_var("APPLE_CODESIGN_ID")
+    installed = cmd_utils.run(
+        "security find-identity -v -p codesigning", get_output=True
+    )
+    print(f"Installed certificates:\n{installed.stdout}")
+    if codesign_id not in installed.stdout:
+        raise ValueError(f"Certificate not installed: {codesign_id}")
 
     print(f"Signing bundle {app_path}...")
     sys.stdout.flush()
