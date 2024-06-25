6ae20283a297ed19290a7506abe172beb81a0322
Nick Bolton <nick@symless.com>
2024-06-25 22:15:11 +0100

    Handle empty version env var

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -2,12 +2,12 @@
 # Gets the version number either from an env var or from the VERSION file.
 #
 
-if (DEFINED ENV{SYNERGY_VERSION})
-    set (VERSION $ENV{SYNERGY_VERSION})
-else()
+set (VERSION $ENV{SYNERGY_VERSION})
+string(STRIP "${VERSION}" VERSION)
+
+if (NOT VERSION)
     file(READ "${CMAKE_SOURCE_DIR}/VERSION" VERSION)
 endif()
 
-string(STRIP "${VERSION}" VERSION)
 message (STATUS "Version number: " ${VERSION})
 add_definitions (-DSYNERGY_VERSION="${VERSION}")
