08716bd8bdadcc496b18f0709b6dd117c9187d58
Nick Bolton <nick@symless.com>
2024-06-25 21:39:28 +0100

    Make CMake version file even simpler

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,52 +1,13 @@
 #
-# Parses the VERSION file, sets the version number define, and creates a .env.version file.
+# Gets the version number either from an env var or from the VERSION file.
 #
 
-file(READ "${CMAKE_SOURCE_DIR}/VERSION" VERSION_CONTENTS)
-string(STRIP "${VERSION_CONTENTS}" VERSION_CONTENTS)
-string(REGEX MATCH "^([0-9]+)\\.([0-9]+)\\.([0-9]+)-*(.*)$" _ ${VERSION_CONTENTS})
-
-set(VERSION_MAJOR "${CMAKE_MATCH_1}")
-set(VERSION_MINOR "${CMAKE_MATCH_2}")
-set(VERSION_PATCH "${CMAKE_MATCH_3}")
-set(VERSION_STAGE "${CMAKE_MATCH_4}")
-
-# CI should specify "snapshot" for the stage when not a release build.
-if (DEFINED ENV{SYNERGY_VERSION_STAGE})
-    set (VERSION_STAGE $ENV{SYNERGY_VERSION_STAGE})
-endif()
-
-# CI should provide the version number meta data.
-if (DEFINED ENV{SYNERGY_VERSION_META})
-    set (VERSION_META $ENV{SYNERGY_VERSION_META})
-endif()
-
-if (VERSION_META)
-    set(SYNERGY_VERSION 
-        "${VERSION_MAJOR}."
-        "${VERSION_MINOR}."
-        "${VERSION_PATCH}-"
-        "${VERSION_STAGE}+"
-        "${VERSION_META}"
-    )
+if (DEFINED ENV{SYNERGY_VERSION})
+    set (VERSION $ENV{SYNERGY_VERSION})
 else()
-    set(SYNERGY_VERSION 
-        "${VERSION_MAJOR}."
-        "${VERSION_MINOR}."
-        "${VERSION_PATCH}-"
-        "${VERSION_STAGE}"
-    )
+    file(READ "${CMAKE_SOURCE_DIR}/VERSION" VERSION)
 endif()
 
-
-message (STATUS "Version number: " ${SYNERGY_VERSION})
-message (STATUS "Build date: " ${SYNERGY_BUILD_DATE})
-
-add_definitions (-DSYNERGY_VERSION="${SYNERGY_VERSION}")
-
-file(WRITE ${CMAKE_BINARY_DIR}/.env.version
-    "SYNERGY_VERSION_MAJOR=${VERSION_MAJOR}\n"
-    "SYNERGY_VERSION_MINOR=${VERSION_MINOR}\n"
-    "SYNERGY_VERSION_PATCH=${VERSION_PATCH}\n"
-    "SYNERGY_VERSION_BUILD=${VERSION_BUILD}\n"
-    "SYNERGY_VERSION_STAGE=${VERSION_STAGE}\n")
+string(STRIP "${VERSION}" VERSION)
+message (STATUS "Version number: " ${VERSION})
+add_definitions (-DSYNERGY_VERSION="${VERSION}")
