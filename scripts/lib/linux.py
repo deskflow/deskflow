bf7172e05275ce896d713f8c09de0d914dc6f6b1
Nick Bolton <nick@symless.com>
2024-06-29 08:09:25 +0100

    Make Ubuntu build extra packages

--- scripts/lib/linux.py
+++ scripts/lib/linux.py
@@ -6,24 +6,43 @@ dist_dir = "dist"
 
 
 def package(filename_base):
+    build_extra_packages = env.get_env_bool("LINUX_EXTRA_PACKAGES", False)
 
     distro, distro_like, _distro_version = env.get_linux_distro()
     if not distro_like:
         distro_like = distro
 
+    build_package(filename_base, distro_like)
+
+    if build_extra_packages:
+        build_package(filename_base, build_tgz=True)
+        build_package(filename_base, build_stgz=True)
+
+
+def build_package(filename_base, distro_like=None, build_tgz=False, build_stgz=False):
+
     extension = None
     generator = None
-    if "debian" in distro_like:
-        extension = "deb"
-        generator = "DEB"
-    elif "fedora" in distro_like:
-        extension = "rpm"
-        generator = "RPM"
-    else:
-        extension = "sh"
-        generator = "STGZ"
 
-    print(f"Building package for distro like {distro_like}")
+    if build_tgz:
+        generator = "TGZ"
+        extension = "tar.gz"
+        print("Building package for Linux (tar.gz archive)")
+
+    elif build_stgz:
+        generator = "STGZ"
+        extension = "sh"
+        print("Building package for Linux (self-extracting tar.gz)")
+    else:
+
+        print(f"Building package for distro like {distro_like}")
+
+        if "debian" in distro_like:
+            generator = "DEB"
+            extension = "deb"
+        elif "fedora" in distro_like:
+            generator = "RPM"
+            extension = "rpm"
 
     original_dir = os.getcwd()
     try:
