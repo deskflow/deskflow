1ddebcd5c073cdc10ad3c1266420a66011e27884
Nick Bolton <nick@symless.com>
2024-07-05 14:42:47 +0100

    Test Linux installation

--- scripts/lib/linux.py
+++ scripts/lib/linux.py
@@ -4,6 +4,7 @@ import lib.env as env
 
 dist_dir = "dist"
 build_dir = "build"
+package_name = "synergy"
 
 
 def package(filename_base, build_distro=True, build_tgz=False, build_stgz=False):
@@ -12,7 +13,10 @@ def package(filename_base, build_distro=True, build_tgz=False, build_stgz=False)
     run_package_cmd(cmd)
     package_filename = get_package_filename(extension)
     target_file = f"{filename_base}.{extension}"
-    copy_to_dist_dir(package_filename, target_file)
+    target_path = copy_to_dist_dir(package_filename, target_file)
+
+    if build_distro:
+        test_install(target_path)
 
 
 def get_package_info(build_distro, build_tgz, build_stgz):
@@ -38,7 +42,7 @@ def get_package_info(build_distro, build_tgz, build_stgz):
         if "debian" in distro_like:
             cpack_generator = "DEB"
             file_extension = "deb"
-        elif "fedora" in distro_like or "opensuse" in distro_like:
+        elif is_like_rpm(distro_like):
             cpack_generator = "RPM"
             file_extension = "rpm"
         elif "arch" in distro_like:
@@ -59,7 +63,9 @@ def get_package_info(build_distro, build_tgz, build_stgz):
 def run_package_cmd(command):
     package_user = env.get_env("LINUX_PACKAGE_USER", required=False)
     if package_user:
-        cmd_utils.run(["sudo", "chown", "-R", package_user, "build"], check=True)
+        cmd_utils.run(
+            ["sudo", "chown", "-R", package_user, "build"], check=True, print_cmd=True
+        )
         command = ["sudo", "-u", package_user] + command
 
     cwd = os.getcwd()
@@ -85,3 +91,48 @@ def copy_to_dist_dir(source_file, target_file):
     target_path = f"{dist_dir}/{target_file}"
     print(f"Copying to: {target_path}")
     shutil.copy(source_file, target_path)
+
+    return target_path
+
+
+def is_like_rpm(distro_like):
+    return "fedora" in distro_like or "opensuse" in distro_like
+
+
+def test_install(package_file):
+
+    distro, distro_like, _distro_version = env.get_linux_distro()
+    if not distro_like:
+        distro_like = distro
+
+    install_pre = None
+    remove_pre = None
+    if "debian" in distro_like:
+        install_pre = ["apt", "install", "-f", "-y"]
+        remove_pre = ["apt", "remove", "-y"]
+    elif is_like_rpm(distro_like):
+        install_pre = ["yum", "install", "-y"]
+        remove_pre = ["yum", "remove", "-y"]
+    elif "arch" in distro_like:
+        install_pre = ["pacman", "-U", "--noconfirm"]
+        remove_pre = ["pacman", "-R", "--noconfirm"]
+    else:
+        raise RuntimeError(f"Linux distro not yet supported: {distro_like}")
+
+    has_sudo = cmd_utils.has_command("sudo")
+    sudo = ["sudo"] if has_sudo else []
+
+    print("Testing installation...")
+    cmd_utils.run(
+        sudo + install_pre + [f"./{package_file}"],
+        check=True,
+        print_cmd=True,
+    )
+
+    try:
+        cmd_utils.run("synergys --version", shell=True, check=True, print_cmd=True)
+    except Exception:
+        raise RuntimeError("Unable to verify version")
+    finally:
+        cmd_utils.run(sudo + remove_pre + [package_name], check=True, print_cmd=True)
+        print("Installation test passed")
