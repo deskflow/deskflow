254cdfdde7d577aeb9928b55b8df7dedddcda2bc
Nick Bolton <nick@symless.com>
2024-07-11 16:43:30 +0100

    Convenience wrapper script for tests

---
+++ scripts/tests.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import argparse, os, sys
+import lib.cmd_utils as cmd_utils
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--unittests", action="store_true")
+    parser.add_argument("--integtests", action="store_true")
+    parser.add_argument(
+        "--filter-file",
+        type=str,
+        help="Takes the base filename without extension and uses it as a filter",
+    )
+    parser.add_argument(
+        "--ignore-return-code",
+        action="store_true",
+        help="Ignore the return code of the test command",
+    )
+
+    args = parser.parse_args()
+    binary = get_binary_path(args)
+    if args.filter_file:
+        file_base = os.path.basename(args.filter_file)
+        without_ext = os.path.splitext(file_base)[0]
+        command = [binary, f"--gtest_filter={without_ext}*"]
+    else:
+        command = [binary]
+
+    result = cmd_utils.run(command, print_cmd=True, check=False)
+    if not args.ignore_return_code:
+        sys.exit(result.returncode)
+
+
+def get_binary_path(args):
+    base_dir = "./build/bin"
+    if args.unittests:
+        return f"{base_dir}/unittests"
+    elif args.integtests:
+        return f"{base_dir}/integtests"
+    else:
+        raise RuntimeError("No test type specified")
+
+
+if __name__ == "__main__":
+    main()
