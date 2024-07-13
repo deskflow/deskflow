0add26f123a3148ef14455122d1584a5956c6d42
Nick Bolton <nick@symless.com>
2024-07-13 05:34:33 +0100

    Add args test for log

--- src/test/unittests/base/LogTests.cpp
+++ src/test/unittests/base/LogTests.cpp
@@ -4,12 +4,24 @@
 #include <gtest/gtest.h>
 
 using testing::HasSubstr;
+using testing::internal::CaptureStdout;
+using testing::internal::GetCapturedStdout;
 
 TEST(LogTests, print_simpleString_outputToConsole) {
-  testing::internal::CaptureStdout();
+  CaptureStdout();
 
   Log::getInstance()->print("test file", 1, "test message");
 
-  std::string output = testing::internal::GetCapturedStdout();
+  std::string output = GetCapturedStdout();
   EXPECT_THAT(output, HasSubstr("INFO: test message\n\ttest file:1\n"));
+}
+
+TEST(LogTests, print_withArgs_outputToConsole) {
+  CaptureStdout();
+
+  Log::getInstance()->print("test file", 1, "test %d %.2f %s", 1, 1.234,
+                            "test arg");
+
+  std::string output = GetCapturedStdout();
+  EXPECT_THAT(output, HasSubstr("INFO: test 1 1.23 test arg\n\ttest file:1\n"));
 }
