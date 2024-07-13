80ce7edefb25c6d160d30356000650ffef8678ed
Nick Bolton <nick@symless.com>
2024-07-13 18:44:56 +0100

    Copyright date

--- src/test/unittests/gui/VersionCheckerTests.cpp
+++ src/test/unittests/gui/VersionCheckerTests.cpp
@@ -1,6 +1,6 @@
 /*
  * synergy -- mouse and keyboard sharing utility
- * Copyright (C) 2012 Symless Ltd.
+ * Copyright (C) 2024 Symless Ltd.
  *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
