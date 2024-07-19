15549f29bbd7f7cf962a9af82f53b095baa90f75
Nick Bolton <nick@symless.com>
2024-07-19 13:01:29 +0100

    Improve dev script for daemon

--- scripts/lib/file_utils.py
+++ scripts/lib/file_utils.py
@@ -1,9 +1,6 @@
 import glob, os, shutil, sys
-
-import colorama  # type: ignore
-from colorama import Fore  # type: ignore
-
-colorama.init()
+import lib.env as env
+import lib.colors as colors
 
 
 class CopyOptions:
@@ -24,24 +21,24 @@ def copy(source, target, options):
     context = CopyContext()
     if options.verbose:
         print(f"Copying files from {source} to {target}")
-    for match in glob.glob(source):
 
-        if os.path.isfile(match):
-            copy_file(match, target, options, context)
-        elif os.path.isdir(match):
-            copy_dir(match, target, options, context)
-        else:
-            raise RuntimeError(f"Path {match} is not a file or directory")
+    try:
+        for match in glob.glob(source):
 
-    if context.errors and options.ignore_errors:
-        print(
-            f"{Fore.YELLOW}WARNING:{Fore.RESET} Ignored {context.errors} copy error(s)"
-        )
+            if os.path.isfile(match):
+                copy_file(match, target, options, context)
+            elif os.path.isdir(match):
+                copy_dir(match, target, options, context)
+            else:
+                raise RuntimeError(f"Path {match} is not a file or directory")
+    finally:
+        if context.errors and options.ignore_errors:
+            print(f"{colors.WARNING_TEXT} Ignored {context.errors} copy error(s)")
 
-    if context.permission_error:
-        print(
-            f"{Fore.BLUE}HINT:{Fore.RESET} A permission error may mean that the file is in use"
-        )
+        if context.permission_error and env.is_windows():
+            print(
+                f"{colors.HINT_TEXT} A Windows file permission error may mean that the file is in use"
+            )
 
 
 def copy_dir(match, target, options, context):
@@ -50,12 +47,8 @@ def copy_dir(match, target, options, context):
 
     try:
         shutil.copytree(match, target, dirs_exist_ok=True)
-
-    except PermissionError as e:
-        context.permission_error = True
-        handle_copy_error(e, options, context)
     except Exception as e:
-        handle_copy_error(e, options, context)
+        handle_all_copy_errors(e, options, context)
 
 
 def copy_file(match, target, options, context):
@@ -64,17 +57,33 @@ def copy_file(match, target, options, context):
 
     try:
         shutil.copy(match, target)
-    except PermissionError as e:
-        context.permission_error = True
-        handle_copy_error(e, options, context)
     except Exception as e:
-        handle_copy_error(e, options, context)
+        handle_all_copy_errors(e, options, context)
 
 
-def handle_copy_error(e, options, context):
-    context.errors += 1
+def handle_all_copy_errors(error, options, context):
+
+    if isinstance(error, shutil.Error):
+        for _, _, file_error in error.args[0]:
+            handle_copy_error(file_error, options, context)
+    else:
+        handle_copy_error(error, options, context)
 
     if not options.ignore_errors:
-        raise e
-    else:
-        print(f"{Fore.YELLOW}WARNING:{Fore.RESET} Copy failed: {e}", file=sys.stderr)
+        raise error
+
+
+def handle_copy_error(error, options, context):
+    if isinstance(error, PermissionError):
+        context.permission_error = True
+
+    if isinstance(error, str):
+        context.permission_error = error.startswith("[Errno 13] Permission denied")
+
+    context.errors += 1
+
+    if options.ignore_errors:
+        print(
+            f"{colors.WARNING_TEXT} Copy failed: {error}",
+            file=sys.stderr,
+        )
