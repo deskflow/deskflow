ef5744781a430499afa5e0c66c97a2e0ecb8daf2
Nick Bolton <nick@symless.com>
2024-07-23 18:43:27 +0100

    Group getters and setters

--- src/gui/src/AppConfig.cpp
+++ src/gui/src/AppConfig.cpp
@@ -109,50 +109,6 @@ void AppConfig::load() {
   }
 }
 
-void AppConfig::applyAppSettings() const {
-  QApplication::setQuitOnLastWindowClosed(!m_CloseToTray);
-}
-
-Config &AppConfig::config() { return m_Config; }
-
-const QString &AppConfig::screenName() const { return m_ScreenName; }
-
-int AppConfig::port() const { return m_Port; }
-
-const QString &AppConfig::networkInterface() const { return m_Interface; }
-
-int AppConfig::logLevel() const { return m_LogLevel; }
-
-bool AppConfig::logToFile() const { return m_LogToFile; }
-
-const QString &AppConfig::logFilename() const { return m_LogFilename; }
-
-QString AppConfig::logDir() const {
-  // by default log to home dir
-  return QDir::home().absolutePath() + "/";
-}
-
-void AppConfig::persistLogDir() const {
-  QDir dir = logDir();
-
-  // persist the log directory
-  if (!dir.exists()) {
-    dir.mkpath(dir.path());
-  }
-}
-
-QString AppConfig::logLevelText() const { return logLevelNames[logLevel()]; }
-
-ProcessMode AppConfig::processMode() const {
-  return m_ServiceEnabled ? ProcessMode::kService : ProcessMode::kDesktop;
-}
-
-bool AppConfig::wizardShouldRun() const {
-  return m_WizardLastRun < kWizardVersion;
-}
-
-bool AppConfig::startedBefore() const { return m_StartedBefore; }
-
 void AppConfig::loadSettings() {
   using enum AppConfig::Setting;
 
@@ -287,14 +243,212 @@ void AppConfig::saveSettings() {
   applyAppSettings();
 }
 
+QString AppConfig::settingName(Setting name) {
+  auto index = static_cast<int>(name);
+  return m_SettingsName[index];
+}
+
+template <typename T> void AppConfig::setSetting(Setting name, T value) {
+  m_Config.setSetting(settingName(name), value);
+}
+
+template <typename T> void AppConfig::setCommonSetting(Setting name, T value) {
+  m_Config.setSetting(settingName(name), value, Config::Scope::User);
+  m_Config.setSetting(settingName(name), value, Config::Scope::System);
+}
+
+QVariant AppConfig::loadSetting(Setting name, const QVariant &defaultValue) {
+  return m_Config.loadSetting(settingName(name), defaultValue);
+}
+
+QVariant
+AppConfig::loadCommonSetting(Setting name, const QVariant &defaultValue) const {
+  QVariant result(defaultValue);
+  QString setting(settingName(name));
+
+  if (m_Config.hasSetting(setting)) {
+    result = m_Config.loadSetting(setting, defaultValue);
+  } else if (m_Config.getScope() == Config::Scope::System) {
+    if (m_Config.hasSetting(setting, Config::Scope::User)) {
+      result = m_Config.loadSetting(setting, defaultValue, Config::Scope::User);
+    }
+  } else if (m_Config.hasSetting(setting, Config::Scope::System)) {
+    result = m_Config.loadSetting(setting, defaultValue, Config::Scope::System);
+  }
+
+  return result;
+}
+
+void AppConfig::loadScope(Config::Scope scope) {
+
+  if (m_Config.getScope() != scope) {
+    setDefaultValues();
+    m_Config.setScope(scope);
+    if (m_Config.hasSetting(
+            settingName(Setting::kScreenName), m_Config.getScope())) {
+      // If the user already has settings, then load them up now.
+      m_Config.loadAll();
+    }
+  }
+}
+
+void AppConfig::setDefaultValues() { m_InitiateConnectionFromServer = false; }
+
+void AppConfig::setLoadFromSystemScope(bool value) {
+
+  if (value) {
+    loadScope(Config::Scope::System);
+  } else {
+    loadScope(Config::Scope::User);
+  }
+
+  /*
+   * It's very imprortant to set this variable after loadScope
+   * because during scope loading this variable can be rewritten with old value
+   */
+  m_LoadFromSystemScope = value;
+}
+
+bool AppConfig::isWritable() const { return m_Config.isWritable(); }
+
+bool AppConfig::isSystemScoped() const {
+  return m_Config.getScope() == Config::Scope::System;
+}
+
+template <typename T>
+void AppConfig::setSettingModified(T &variable, const T &newValue) {
+  if (variable != newValue) {
+    variable = newValue;
+    setModified(true);
+  }
+}
+
+void AppConfig::generateCertificate(bool forceGeneration) const {
+  try {
+    SslCertificate sslCertificate;
+    sslCertificate.generateCertificate(
+        tlsCertPath(), tlsKeyLength(), forceGeneration);
+    emit sslToggled();
+  } catch (const std::exception &e) {
+    qDebug() << e.what();
+    qFatal("Failed to configure TLS");
+  }
+}
+
+void AppConfig::applyAppSettings() const {
+  QApplication::setQuitOnLastWindowClosed(!m_CloseToTray);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Begin getters and setters
+///////////////////////////////////////////////////////////////////////////////
+
 #ifdef SYNERGY_ENABLE_LICENSING
+
 bool AppConfig::activationHasRun() const { return m_ActivationHasRun; }
 
 AppConfig &AppConfig::activationHasRun(bool value) {
   m_ActivationHasRun = value;
   return *this;
 }
-#endif
+
+void AppConfig::setEdition(Edition e) {
+  setSettingModified(m_Edition, e);
+  setCommonSetting(Setting::kEditionSetting, m_Edition);
+}
+
+Edition AppConfig::edition() const { return m_Edition; }
+
+void AppConfig::setSerialKey(const QString &serial) {
+  setSettingModified(m_Serialkey, serial);
+  setCommonSetting(Setting::kSerialKey, m_Serialkey);
+}
+
+void AppConfig::clearSerialKey() { m_Serialkey.clear(); }
+
+QString AppConfig::serialKey() const { return m_Serialkey; }
+
+int AppConfig::lastExpiringWarningTime() const {
+  return m_LastExpiringWarningTime;
+}
+
+void AppConfig::setLastExpiringWarningTime(int newValue) {
+  setSettingModified(m_LastExpiringWarningTime, newValue);
+}
+
+#endif // SYNERGY_ENABLE_LICENSING
+
+void AppConfig::setServerGroupChecked(bool newValue) {
+  setSettingModified(m_ServerGroupChecked, newValue);
+}
+
+void AppConfig::setUseExternalConfig(bool newValue) {
+  setSettingModified(m_UseExternalConfig, newValue);
+}
+
+void AppConfig::setConfigFile(const QString &newValue) {
+  setSettingModified(m_ConfigFile, newValue);
+}
+
+void AppConfig::setUseInternalConfig(bool newValue) {
+  setSettingModified(m_UseInternalConfig, newValue);
+}
+
+void AppConfig::setClientGroupChecked(bool newValue) {
+  setSettingModified(m_ClientGroupChecked, newValue);
+}
+
+void AppConfig::setServerHostname(const QString &newValue) {
+  setSettingModified(m_ServerHostname, newValue);
+}
+
+void AppConfig::setClientHostMode(bool newValue) {
+  setSettingModified(m_ClientHostMode, newValue);
+}
+
+void AppConfig::setServerClientMode(bool newValue) {
+  setSettingModified(m_ServerClientMode, newValue);
+}
+
+Config &AppConfig::config() { return m_Config; }
+
+const QString &AppConfig::screenName() const { return m_ScreenName; }
+
+int AppConfig::port() const { return m_Port; }
+
+const QString &AppConfig::networkInterface() const { return m_Interface; }
+
+int AppConfig::logLevel() const { return m_LogLevel; }
+
+bool AppConfig::logToFile() const { return m_LogToFile; }
+
+const QString &AppConfig::logFilename() const { return m_LogFilename; }
+
+QString AppConfig::logDir() const {
+  // by default log to home dir
+  return QDir::home().absolutePath() + "/";
+}
+
+void AppConfig::persistLogDir() const {
+  QDir dir = logDir();
+
+  // persist the log directory
+  if (!dir.exists()) {
+    dir.mkpath(dir.path());
+  }
+}
+
+QString AppConfig::logLevelText() const { return logLevelNames[logLevel()]; }
+
+ProcessMode AppConfig::processMode() const {
+  return m_ServiceEnabled ? ProcessMode::kService : ProcessMode::kDesktop;
+}
+
+bool AppConfig::wizardShouldRun() const {
+  return m_WizardLastRun < kWizardVersion;
+}
+
+bool AppConfig::startedBefore() const { return m_StartedBefore; }
 
 QString AppConfig::lastVersion() const { return m_LastVersion; }
 
@@ -332,32 +486,6 @@ void AppConfig::setStartedBefore(bool b) {
 void AppConfig::setElevateMode(ElevateMode em) {
   setSettingModified(m_ElevateMode, em);
 }
-
-#ifdef SYNERGY_ENABLE_LICENSING
-void AppConfig::setEdition(Edition e) {
-  setSettingModified(m_Edition, e);
-  setCommonSetting(Setting::kEditionSetting, m_Edition);
-}
-
-Edition AppConfig::edition() const { return m_Edition; }
-
-void AppConfig::setSerialKey(const QString &serial) {
-  setSettingModified(m_Serialkey, serial);
-  setCommonSetting(Setting::kSerialKey, m_Serialkey);
-}
-
-void AppConfig::clearSerialKey() { m_Serialkey.clear(); }
-
-QString AppConfig::serialKey() const { return m_Serialkey; }
-
-int AppConfig::lastExpiringWarningTime() const {
-  return m_LastExpiringWarningTime;
-}
-
-void AppConfig::setLastExpiringWarningTime(int newValue) {
-  setSettingModified(m_LastExpiringWarningTime, newValue);
-}
-#endif
 
 QString AppConfig::coreServerName() const { return m_CoreServerName; }
 
@@ -442,130 +570,6 @@ void AppConfig::setPreventSleep(bool newValue) {
 
 bool AppConfig::minimizeToTray() const { return m_MinimizeToTray; }
 
-QString AppConfig::settingName(Setting name) {
-  auto index = static_cast<int>(name);
-  return m_SettingsName[index];
-}
-
-template <typename T> void AppConfig::setSetting(Setting name, T value) {
-  m_Config.setSetting(settingName(name), value);
-}
-
-template <typename T> void AppConfig::setCommonSetting(Setting name, T value) {
-  m_Config.setSetting(settingName(name), value, Config::Scope::User);
-  m_Config.setSetting(settingName(name), value, Config::Scope::System);
-}
-
-QVariant AppConfig::loadSetting(Setting name, const QVariant &defaultValue) {
-  return m_Config.loadSetting(settingName(name), defaultValue);
-}
-
-QVariant
-AppConfig::loadCommonSetting(Setting name, const QVariant &defaultValue) const {
-  QVariant result(defaultValue);
-  QString setting(settingName(name));
-
-  if (m_Config.hasSetting(setting)) {
-    result = m_Config.loadSetting(setting, defaultValue);
-  } else if (m_Config.getScope() == Config::Scope::System) {
-    if (m_Config.hasSetting(setting, Config::Scope::User)) {
-      result = m_Config.loadSetting(setting, defaultValue, Config::Scope::User);
-    }
-  } else if (m_Config.hasSetting(setting, Config::Scope::System)) {
-    result = m_Config.loadSetting(setting, defaultValue, Config::Scope::System);
-  }
-
-  return result;
-}
-
-void AppConfig::loadScope(Config::Scope scope) {
-
-  if (m_Config.getScope() != scope) {
-    setDefaultValues();
-    m_Config.setScope(scope);
-    if (m_Config.hasSetting(
-            settingName(Setting::kScreenName), m_Config.getScope())) {
-      // If the user already has settings, then load them up now.
-      m_Config.loadAll();
-    }
-  }
-}
-
-void AppConfig::setDefaultValues() { m_InitiateConnectionFromServer = false; }
-
-void AppConfig::setLoadFromSystemScope(bool value) {
-
-  if (value) {
-    loadScope(Config::Scope::System);
-  } else {
-    loadScope(Config::Scope::User);
-  }
-
-  /*
-   * It's very imprortant to set this variable after loadScope
-   * because during scope loading this variable can be rewritten with old value
-   */
-  m_LoadFromSystemScope = value;
-}
-
-bool AppConfig::isWritable() const { return m_Config.isWritable(); }
-
-bool AppConfig::isSystemScoped() const {
-  return m_Config.getScope() == Config::Scope::System;
-}
-
-bool AppConfig::serverGroupChecked() const { return m_ServerGroupChecked; }
-
-bool AppConfig::useExternalConfig() const { return m_UseExternalConfig; }
-
-const QString &AppConfig::configFile() const { return m_ConfigFile; }
-
-bool AppConfig::useInternalConfig() const { return m_UseInternalConfig; }
-
-bool AppConfig::clientGroupChecked() const { return m_ClientGroupChecked; }
-
-QString AppConfig::serverHostname() const { return m_ServerHostname; }
-
-void AppConfig::setServerGroupChecked(bool newValue) {
-  setSettingModified(m_ServerGroupChecked, newValue);
-}
-
-void AppConfig::setUseExternalConfig(bool newValue) {
-  setSettingModified(m_UseExternalConfig, newValue);
-}
-
-void AppConfig::setConfigFile(const QString &newValue) {
-  setSettingModified(m_ConfigFile, newValue);
-}
-
-void AppConfig::setUseInternalConfig(bool newValue) {
-  setSettingModified(m_UseInternalConfig, newValue);
-}
-
-void AppConfig::setClientGroupChecked(bool newValue) {
-  setSettingModified(m_ClientGroupChecked, newValue);
-}
-
-void AppConfig::setServerHostname(const QString &newValue) {
-  setSettingModified(m_ServerHostname, newValue);
-}
-
-void AppConfig::setClientHostMode(bool newValue) {
-  setSettingModified(m_ClientHostMode, newValue);
-}
-
-void AppConfig::setServerClientMode(bool newValue) {
-  setSettingModified(m_ServerClientMode, newValue);
-}
-
-template <typename T>
-void AppConfig::setSettingModified(T &variable, const T &newValue) {
-  if (variable != newValue) {
-    variable = newValue;
-    setModified(true);
-  }
-}
-
 void AppConfig::setTlsCertPath(const QString &path) { m_TlsCertPath = path; }
 
 QString AppConfig::tlsCertPath() const { return m_TlsCertPath; }
@@ -576,18 +580,6 @@ void AppConfig::setTlsKeyLength(const QString &length) {
   if (m_TlsKeyLength != length) {
     m_TlsKeyLength = length;
     generateCertificate(true);
-  }
-}
-
-void AppConfig::generateCertificate(bool forceGeneration) const {
-  try {
-    SslCertificate sslCertificate;
-    sslCertificate.generateCertificate(
-        tlsCertPath(), tlsKeyLength(), forceGeneration);
-    emit sslToggled();
-  } catch (const std::exception &e) {
-    qDebug() << e.what();
-    qFatal("Failed to configure TLS");
   }
 }
 
@@ -602,3 +594,19 @@ void AppConfig::setCloseToTray(bool minimize) {
 }
 
 bool AppConfig::closeToTray() const { return m_CloseToTray; }
+
+bool AppConfig::serverGroupChecked() const { return m_ServerGroupChecked; }
+
+bool AppConfig::useExternalConfig() const { return m_UseExternalConfig; }
+
+const QString &AppConfig::configFile() const { return m_ConfigFile; }
+
+bool AppConfig::useInternalConfig() const { return m_UseInternalConfig; }
+
+bool AppConfig::clientGroupChecked() const { return m_ClientGroupChecked; }
+
+QString AppConfig::serverHostname() const { return m_ServerHostname; }
+
+///////////////////////////////////////////////////////////////////////////////
+// End getters and setters
+///////////////////////////////////////////////////////////////////////////////
