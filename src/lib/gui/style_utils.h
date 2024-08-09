ecb1416e97c71011f46d21e9f6e21d44545afe52
Nick Bolton <nick@symless.com>
2024-08-10 00:23:11 +0100

    Simplify dark mode code

--- src/lib/gui/style_utils.h
+++ src/lib/gui/style_utils.h
@@ -29,8 +29,7 @@ namespace synergy::gui {
  */
 inline bool isDarkMode() {
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
-  const auto hints = QGuiApplication::styleHints();
-  const auto scheme = hints->colorScheme();
+  const auto scheme = QGuiApplication::styleHints()->colorScheme();
   return scheme == Qt::ColorScheme::Dark;
 #else
   const QPalette defaultPalette;
