9cff829d55b4142fa4cd4286401d22b7b1e49370
Nick Bolton <nick@symless.com>
2024-09-03 20:11:13 +0100

    Brighter red

--- scripts/lib/colors.py
+++ scripts/lib/colors.py
@@ -4,6 +4,6 @@ from colorama import Fore  # type: ignore
 colorama.init()
 
 SUCCESS_TEXT = f"{Fore.LIGHTGREEN_EX}Success:{Fore.RESET}"
-ERROR_TEXT = f"{Fore.RED}Error:{Fore.RESET}"
+ERROR_TEXT = f"{Fore.LIGHTRED_EX}Error:{Fore.RESET}"
 WARNING_TEXT = f"{Fore.LIGHTYELLOW_EX}Warning:{Fore.RESET}"
 HINT_TEXT = f"{Fore.LIGHTBLUE_EX}Hint:{Fore.RESET}"
