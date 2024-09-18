660fb9daff5fcb5e95fa49a0a2f9fca3d6e8da6f
Chris Rizzitello <sithlord48@gmail.com>
2024-09-18 13:25:20 -0400

    fix: protocol compatibility with Synergy (#7524)
    * fix: protocol compatiblity wity synergy
    
    * Apply clang format
    
    * Update ChangeLog
    
    ---------
    
    Co-authored-by: Nick Bolton <nick@symless.com>

--- src/lib/deskflow/protocol_types.cpp
+++ src/lib/deskflow/protocol_types.cpp
@@ -18,8 +18,9 @@
 
 #include "deskflow/protocol_types.h"
 
-const char *const kMsgHello = "Deskflow%2i%2i";
-const char *const kMsgHelloBack = "Deskflow%2i%2i%s";
+// Keep the "Synergy" name for hello to preserve compatibility with Synergy
+const char *const kMsgHello = "Synergy%2i%2i";
+const char *const kMsgHelloBack = "Synergy%2i%2i%s";
 const char *const kMsgCNoop = "CNOP";
 const char *const kMsgCClose = "CBYE";
 const char *const kMsgCEnter = "CINN%2i%2i%4i%2i";
