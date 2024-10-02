6ca339bd0a9ccde75ec9ae7de84fe2735cbb7c94
Nick Bolton <nick@symless.com>
2024-10-02 17:29:14 +0100

    Switch on `BUILD_UNIFIED` for post-build

--- cmake/Build.cmake
+++ cmake/Build.cmake
@@ -58,12 +58,17 @@ macro(post_config_all)
       VERBATIM
       COMMENT "Copying files to bin dir")
 
-    add_dependencies(
-      run_post_build
-      synergy
-      synergyc
-      synergys
-      synergyd)
+    if(BUILD_UNIFIED)
+      add_dependencies(run_post_build synergy synergy-core synergyd)
+    else()
+      add_dependencies(
+        run_post_build
+        synergy
+        synergyc
+        synergys
+        synergyd)
+    endif()
+
   endif()
 
 endmacro()
