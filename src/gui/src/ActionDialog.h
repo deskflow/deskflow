7b3d8c3dc3d9d284ecad54942880ef2f9f1df4a4
sithlord48 <sithlord48@gmail.com>
2024-10-18 18:14:46 -0400

    refactor: actiondialog to not have ui as base of the class

--- src/gui/src/ActionDialog.h
+++ src/gui/src/ActionDialog.h
@@ -20,36 +20,38 @@
 
 #include <QDialog>
 
-#include "ui_ActionDialogBase.h"
-
 class Hotkey;
 class Action;
 class QRadioButton;
 class QButtonGroup;
 class ServerConfig;
+class KeySequenceWidget;
 
-class ActionDialog : public QDialog, public Ui::ActionDialogBase
+namespace Ui {
+class ActionDialog;
+}
+
+class ActionDialog : public QDialog
 {
   Q_OBJECT
 
 public:
   ActionDialog(QWidget *parent, ServerConfig &config, Hotkey &hotkey, Action &action);
+  ~ActionDialog() override;
 
 protected slots:
-  void accept();
+  void accept() override;
   void on_m_pKeySequenceWidgetHotkey_keySequenceChanged();
 
 protected:
-  const KeySequenceWidget *sequenceWidget() const
-  {
-    return m_pKeySequenceWidgetHotkey;
-  }
+  const KeySequenceWidget *sequenceWidget() const;
   const ServerConfig &serverConfig() const
   {
     return m_ServerConfig;
   }
 
 private:
+  std::unique_ptr<Ui::ActionDialog> ui;
   const ServerConfig &m_ServerConfig;
   Hotkey &m_Hotkey;
   Action &m_Action;
