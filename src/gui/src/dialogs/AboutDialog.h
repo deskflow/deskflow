155e4d6b3273d32e6b370c8de79c16c66da04d38
sithlord48 <sithlord48@gmail.com>
2024-10-30 23:04:26 -0400

    refactor: ui less aboutDialog

--- src/gui/src/dialogs/AboutDialog.h
+++ src/gui/src/dialogs/AboutDialog.h
@@ -1,5 +1,6 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * Copyright (C) 2024 Chris Rizzitello <sithlord48@gmail.com>
  * Copyright (C) 2012 Symless Ltd.
  * Copyright (C) 2008 Volker Lanz (vl@fidra.de)
  *
@@ -18,32 +19,60 @@
 
 #pragma once
 
-#include "ui_AboutDialogBase.h"
-
 #include <QDialog>
-#include <QMainWindow>
 
-class QWidget;
-class QString;
-
-class AboutDialog : public QDialog, public Ui::AboutDialogBase
+class AboutDialog : public QDialog
 {
   Q_OBJECT
-
 public:
-  struct Deps
-  {
-    virtual ~Deps() = default;
-    virtual bool isDarkMode() const;
-  };
-
-  explicit AboutDialog(QMainWindow *parent, std::shared_ptr<Deps> deps = std::make_shared<Deps>());
-  int exec() override;
+  explicit AboutDialog(QWidget *parent = nullptr);
+  ~AboutDialog() = default;
 
 private:
-  std::shared_ptr<Deps> m_pDeps;
+  inline static const auto s_lightCopy = QStringLiteral(":/icons/64x64/copy-light.png");
+  inline static const auto s_darkCopy = QStringLiteral(":/icons/64x64/copy-dark.png");
+  inline static const auto s_lightLogo = QStringLiteral(":/image/logo-light.png");
+  inline static const auto s_darkLogo = QStringLiteral(":/image/logo-dark.png");
+  inline static const auto s_awesomeDevs = QStringList{
+      // Chris is the ultimate creator, and the one who started it all in 2001.
+      QStringLiteral("Chris Schoeneman"),
 
-  void updateLogo() const;
-  void setLogo(const char *const &filename) const;
-  QString importantDevelopers() const;
+      // Richard and Adam developed CosmoSynergy, the 90's predecessor project.
+      QStringLiteral("Richard Lee"),
+      QStringLiteral("Adam Feder"),
+
+      // Nick continued the legacy in 2009 started by Chris.
+      QStringLiteral("Nick Bolton"),
+
+      // Volker wrote the first version of the GUI (QSynergy) in 2008.
+      QStringLiteral("Volker Lanz"),
+
+      // Re-ignited the project in 2008 and rebuilt the community.
+      QStringLiteral("Sorin Sbârnea"),
+
+      // Contributors of bug fixes in the early days.
+      QStringLiteral("Ryan Breen"),
+      QStringLiteral("Guido Poschta"),
+      QStringLiteral("Bertrand Landry Hetu"),
+      QStringLiteral("Tom Chadwick"),
+      QStringLiteral("Brent Priddy"),
+      QStringLiteral("Jason Axelson"),
+      QStringLiteral("Jake Petroules"),
+
+      // Implemented Wayland support (libei and libportal).
+      QStringLiteral("Peter Hutterer"),
+      QStringLiteral("Olivier Fourdan"),
+
+      // Symless employees (in order of joining).
+      QStringLiteral("Kyle Bloom"),
+      QStringLiteral("Daun Chung"),
+      QStringLiteral("Serhii Hadzhylov"),
+      QStringLiteral("Oleksandr Lysytsia"),
+      QStringLiteral("Olena Kutytska"),
+      QStringLiteral("Owen Phillips"),
+      QStringLiteral("Daniel Evenson"),
+
+      // Prominent Deskflow development community members
+      QStringLiteral("Chris Rizzitello"),
+  };
 };
