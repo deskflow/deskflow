7f9fd80f986e8208cda31d05112c7dc47f8de913
sithlord48 <sithlord48@gmail.com>
2024-11-27 00:14:32 -0500

    refactor: remove workarounds for unsupported ms vc versions

--- src/lib/common/stdistream.h
+++ src/lib/common/stdistream.h
@@ -26,31 +26,3 @@
 #endif
 
 #include "stdpost.h"
-
-#if defined(_MSC_VER) && _MSC_VER <= 1200
-// VC++6 istream has no overloads for __int* types, .NET does
-inline std::istream &operator>>(std::istream &s, SInt8 &i)
-{
-  return s >> (signed char &)i;
-}
-inline std::istream &operator>>(std::istream &s, SInt16 &i)
-{
-  return s >> (short &)i;
-}
-inline std::istream &operator>>(std::istream &s, SInt32 &i)
-{
-  return s >> (int &)i;
-}
-inline std::istream &operator>>(std::istream &s, UInt8 &i)
-{
-  return s >> (unsigned char &)i;
-}
-inline std::istream &operator>>(std::istream &s, UInt16 &i)
-{
-  return s >> (unsigned short &)i;
-}
-inline std::istream &operator>>(std::istream &s, UInt32 &i)
-{
-  return s >> (unsigned int &)i;
-}
-#endif
