7d05bf90ea6bc239e7b7f82cb4e226e89206be31
sithlord48 <sithlord48@gmail.com>
2024-11-28 07:28:55 -0500

    refactor: use enum for connections security level
     based on barrier: 5c7d719

---
+++ src/lib/net/SecurityLevel.h
@@ -0,0 +1,17 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2024 Deskflow Developers
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#pragma once
+
+/**
+ * \enum SecurityLevel
+ * \brief This enum is used to set how the client and server will communicate.
+ */
+enum class SecurityLevel
+{
+  PlainText, /** The Connection is not encrypted */
+  Encrypted  /** The Connection is encrypted */
+};
