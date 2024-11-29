93afae5ea86b056fb71b9a34ffa254dd56472696
sithlord48 <sithlord48@gmail.com>
2024-11-29 12:03:51 -0500

    feat: add config for client cert required
     based on barrier: 8bc280e0

--- src/test/shared/gui/mocks/AppConfigMock.h
+++ src/test/shared/gui/mocks/AppConfigMock.h
@@ -61,6 +61,7 @@ public:
   MOCK_METHOD(bool, isActiveScopeSystem, (), (const, override));
   MOCK_METHOD(bool, isActiveScopeWritable, (), (const, override));
   MOCK_METHOD(bool, clientGroupChecked, (), (const, override));
+  MOCK_METHOD(bool, requireClientCerts, (), (const, override));
 
   //
   // Setters
@@ -84,6 +85,7 @@ public:
   MOCK_METHOD(void, setEnableService, (bool enableService), (override));
   MOCK_METHOD(void, setCloseToTray, (bool closeToTray), (override));
   MOCK_METHOD(void, setInvertConnection, (bool invertConnection), (override));
+  MOCK_METHOD(void, setRequireClientCerts, (bool requireClientCerts), (override));
 
 private:
   const QString m_stub = "stub";
