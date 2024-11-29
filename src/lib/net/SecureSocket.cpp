f9d6fd641da075e32c12d41eb7a974e0242738b4
sithlord48 <sithlord48@gmail.com>
2024-11-29 11:05:25 -0500

    fix: Attempt to load SSL certs when connecting  based on barrier: c0ce8937, 4d73ed9f

--- src/lib/net/SecureSocket.cpp
+++ src/lib/net/SecureSocket.cpp
@@ -454,6 +454,15 @@ int SecureSocket::secureConnect(int socket)
 {
   std::lock_guard<std::mutex> ssl_lock{ssl_mutex_};
 
+  std::string certDir =
+      deskflow::string::sprintf("%s/%s/%s", ARCH->getProfileDirectory().c_str(), kSslDir, kCertificateFilename);
+
+  if (!loadCertificates(certDir)) {
+    LOG((CLOG_ERR "could not load client certificates"));
+    disconnect();
+    return -1;
+  }
+
   createSSL();
 
   // attach the socket descriptor
