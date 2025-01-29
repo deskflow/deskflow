77b3a8c618764ca66c5d0d51eec5252471784325
sithlord48 <sithlord48@gmail.com>
2025-01-28 20:33:26 -0500

    chore: remove unused ARCH::vsnprintf

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 Deskflow Developers
+# SPDX-FileCopyrightText: 2024 - 2025 Deskflow Developers
 # SPDX-FileCopyrightText: 2024 Symless Ltd
 # SPDX-License-Identifier: MIT
 
@@ -124,7 +124,6 @@ macro(configure_unix_libs)
   check_function_exists(gmtime_r HAVE_GMTIME_R)
   check_function_exists(nanosleep HAVE_NANOSLEEP)
   check_function_exists(sigwait HAVE_POSIX_SIGWAIT)
-  check_function_exists(vsnprintf HAVE_VSNPRINTF)
   check_function_exists(inet_aton HAVE_INET_ATON)
 
   # For some reason, the check_function_exists macro doesn't detect the
--- src/lib/arch/win32/ArchStringWindows.cpp
+++ src/lib/arch/win32/ArchStringWindows.cpp
@@ -1,5 +1,6 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Developers
  * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
  * SPDX-FileCopyrightText: (C) 2002 Chris Schoeneman
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
@@ -14,11 +15,6 @@
 //
 // ArchStringWindows
 //
-
-#include "arch/multibyte.h"
-#define HAVE_VSNPRINTF 1
-#define ARCH_VSNPRINTF _vsnprintf
-#include "arch/vsnprintf.h"
 
 ArchStringWindows::ArchStringWindows()
 {
