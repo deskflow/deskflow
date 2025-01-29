d5be2ec5b364e6e039285e54b392ca5938d99b27
sithlord48 <sithlord48@gmail.com>
2025-01-28 20:13:02 -0500

    refactor: move SecureSocket::formatFingerprint -> SecureUtils std::string formatSSLFingerprint based on https://github.com/debauchee/barrier/commit/0e406d491823bfc9dfed0fcc7934cdece8db7dd0          https://github.com/debauchee/barrier/commit/9d8e1faf59bbbc0360adc52b964d71a510f4e8a2          https://github.com/debauchee/barrier/commit/b793675ef8cbf7f69fe8ba7cbdf3689e7f60c657

---
+++ src/lib/net/SecureUtils.cpp
@@ -0,0 +1,30 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Devlopers
+ * SPDX-FileCopyrightText: (C) 2021 Barrier Contributors
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#include "SecureUtils.h"
+#include "base/String.h"
+
+namespace deskflow {
+
+std::string formatSSLFingerprint(const std::string &fingerprint, bool convertToHex, bool enableSeparators)
+{
+  std::string rtn = fingerprint;
+
+  if (convertToHex)
+    rtn = deskflow::string::toHex(fingerprint, 2);
+
+  deskflow::string::uppercase(rtn);
+
+  if (enableSeparators) {
+    size_t separators = rtn.size() / 2;
+    for (size_t i = 1; i < separators; i++)
+      rtn.insert(i * 3 - 1, ":");
+  }
+  return rtn;
+}
+
+} // namespace deskflow
