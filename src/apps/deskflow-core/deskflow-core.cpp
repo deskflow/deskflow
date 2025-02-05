8cd945c7315970582d2d2be9f163b01f1b47d755
Nick Bolton <nick@symless.com>
2025-02-05 18:09:29 +0000

    refactor: Move daemon command to core binary

--- src/apps/deskflow-core/deskflow-core.cpp
+++ src/apps/deskflow-core/deskflow-core.cpp
@@ -1,7 +1,7 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
  * SPDX-FileCopyrightText: (C) 2025 Chris Rizzitello <sithlord48@gmail.com>
- * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
+ * SPDX-FileCopyrightText: (C) 2012 - 2025 Symless Ltd.
  * SPDX-FileCopyrightText: (C) 2002 Chris Schoeneman
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
  */
@@ -10,6 +10,7 @@
 #include "base/EventQueue.h"
 #include "base/Log.h"
 #include "deskflow/ClientApp.h"
+#include "deskflow/DaemonApp.h"
 #include "deskflow/ServerApp.h"
 
 #if SYSAPI_WIN32
@@ -21,9 +22,15 @@
 void showHelp()
 {
   std::cout << "Usage: deskflow-core <server | client> [...options]" << std::endl;
-  std::cout << "server - start as a server (deskflow-server)" << std::endl;
-  std::cout << "client - start as a client (deskflow-client)" << std::endl;
+  std::cout << "server - start as a server" << std::endl;
+  std::cout << "client - start as a client" << std::endl;
+
+#ifdef SYSAPI_WIN32
+  std::cout << "daemon - start as a demon (Windows only)" << std::endl;
+  std::cout << "use deskflow-core <server|client|daemon> --help for more information." << std::endl;
+#else
   std::cout << "use deskflow-core <server|client> --help for more information." << std::endl;
+#endif
 }
 
 bool isServer(int argc, char **argv)
@@ -34,6 +41,11 @@ bool isServer(int argc, char **argv)
 bool isClient(int argc, char **argv)
 {
   return (argc > 1 && argv[1] == std::string("client"));
+}
+
+bool isDaemon(int argc, char **argv)
+{
+  return (argc > 1 && argv[1] == std::string("daemon"));
 }
 
 int main(int argc, char **argv)
@@ -48,7 +60,15 @@ int main(int argc, char **argv)
   Log log;
   EventQueue events;
 
-  if (isServer(argc, argv)) {
+  if (isDaemon(argc, argv)) {
+#ifdef SYSAPI_WIN32
+    DaemonApp app;
+    return app.run(argc, argv);
+#else
+    showHelp();
+    return kExitArgs;
+#endif
+  } else if (isServer(argc, argv)) {
     ServerApp app(&events);
     return app.run(argc, argv);
   } else if (isClient(argc, argv)) {
