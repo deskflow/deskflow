ab118a3aa3f2b9e0ddee4dd6539e8e04f454535b
sithlord48 <sithlord48@gmail.com>
2025-02-19 20:00:55 -0500

    chore: remove unused style items

--- src/lib/gui/styles.h
+++ src/lib/gui/styles.h
@@ -11,11 +11,9 @@
 namespace deskflow::gui {
 
 const auto kColorWhite = "#ffffff";
-const auto kColorPrimary = "#ff7c00";
 const auto kColorSecondary = "#4285f4";
 const auto kColorTertiary = "#33b2cc";
 const auto kColorError = "#ec4c47";
-const auto kColorNotice = "#3b67d3";
 const auto kColorLightGrey = "#666666";
 
 const auto kStyleLink = //
@@ -32,10 +30,5 @@ const auto kStyleErrorActiveLabel = //
 const auto kStyleErrorInactiveLabel = //
     QString("padding: 3px 5px; border-radius: 3px;"
             "background-color: none");
-
-const auto kStyleNoticeLabel = //
-    QString("padding: 3px 5px; border-radius: 3px;"
-            "background-color: %1; color: %2")
-        .arg(kColorNotice, kColorWhite);
 
 } // namespace deskflow::gui
