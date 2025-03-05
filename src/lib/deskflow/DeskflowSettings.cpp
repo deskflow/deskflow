b9aef3179deb72af02392e90dd0847dd6ac3d5f5
sithlord48 <sithlord48@gmail.com>
2025-03-04 21:06:41 -0500

    Start Deskflow Settings

---
+++ src/lib/deskflow/DeskflowSettings.cpp
@@ -0,0 +1,127 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Chris Rizzitello <sithlord48@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#include "DeskflowSettings.h"
+
+#include <QFile>
+
+DeskflowSettings *DeskflowSettings::instance()
+{
+  static DeskflowSettings m;
+  return &m;
+}
+
+DeskflowSettings::DeskflowSettings(QObject *parent) : QObject(parent)
+{
+  initSettings();
+}
+
+void DeskflowSettings::initSettings()
+{
+  if (!userSettings) {
+    userSettings = new QSettings(Settings::UserSettingPath, QSettings::IniFormat);
+    qInfo() << "userSettings " << userSettings->fileName();
+  }
+
+  if (!systemSettings) {
+    systemSettings = new QSettings(Settings::SystemSettingPath, QSettings::IniFormat);
+    qInfo() << "systemSettings " << systemSettings->fileName();
+  }
+
+  // Only one will be active at anytime, UserScope == 0
+  const auto userScopeCheck = !userSettings->value(Settings::Core::Scope).toBool();
+  const auto systemScopeCheck = !systemSettings->value(Settings::Core::Scope).toBool();
+  if (userScopeCheck && systemScopeCheck)
+    m_settings = userSettings;
+  else
+    m_settings = systemSettings;
+}
+
+void DeskflowSettings::cleanSettings()
+{
+  const QStringList keys = m_settings->allKeys();
+  for (const QString &key : keys) {
+    if (!validKeys.contains(key))
+      m_settings->remove(key);
+    if (m_settings->value(key).toString().isEmpty() && !m_settings->value(key).isValid())
+      m_settings->remove(key);
+  }
+}
+
+QVariant DeskflowSettings::defaultValue(const QString &key)
+{
+  if (key == Settings::Core::Scope) {
+    return false;
+  }
+
+  return QVariant();
+}
+
+bool DeskflowSettings::isWritable()
+{
+  return instance()->m_settings->isWritable();
+}
+
+bool DeskflowSettings::isSystemScope()
+{
+  return instance()->m_settings->scope() == QSettings::SystemScope;
+}
+
+void DeskflowSettings::setScope(bool systemScope)
+{
+  auto scope = systemScope ? QSettings::SystemScope : QSettings::UserScope;
+  if (instance()->m_settings->scope() == scope)
+    return;
+
+  const bool wasWritable = instance()->m_settings->isWritable();
+
+  instance()->userSettings->setValue(Settings::Core::Scope, scope);
+  instance()->userSettings->sync();
+
+  instance()->systemSettings->setValue(Settings::Core::Scope, scope);
+  instance()->systemSettings->sync();
+
+  instance()->initSettings();
+
+  const bool isWritable = instance()->m_settings->isWritable();
+
+  if (isWritable != wasWritable)
+    Q_EMIT instance()->writableChanged(isWritable);
+
+  Q_EMIT instance()->scopeChanged(scope == QSettings::SystemScope);
+}
+
+void DeskflowSettings::setValue(const QString &key, const QVariant &value)
+{
+  if (instance()->m_settings->value(key) == value)
+    return;
+
+  if (!value.isValid())
+    instance()->m_settings->remove(key);
+  else if (key == Settings::Core::Scope) {
+    instance()->setScope(value.value<QSettings::Scope>());
+  } else {
+    instance()->m_settings->setValue(key, value);
+  }
+
+  instance()->m_settings->sync();
+  Q_EMIT instance()->settingsChanged();
+}
+
+QVariant DeskflowSettings::value(const QString &key)
+{
+  return instance()->m_settings->value(key, defaultValue(key));
+}
+
+void DeskflowSettings::restoreDefaultSettings()
+{
+  for (const auto &key : instance()->validKeys) {
+    instance()->blockSignals(true);
+    instance()->m_settings->setValue(key, defaultValue(key));
+    instance()->blockSignals(false);
+    Q_EMIT instance()->settingsChanged();
+  }
+}
---
+++ src/lib/deskflow/DeskflowSettings.h
@@ -0,0 +1,73 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Chris Rizzitello <sithlord48@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#pragma once
+
+#include "common/constants.h"
+
+#include <QCoreApplication>
+#include <QDir>
+#include <QObject>
+#include <QSettings>
+#include <QVariant>
+
+/**
+ * @brief The Settings struct contains all the static path info for the various keys and setting files
+ */
+struct Settings
+{
+#if defined(Q_OS_WIN)
+  inline const static auto UserSettingPath =
+      QStringLiteral("%1/AppData/Local/%2/%3.ini").arg(QDir::homePath(), kAppName, kAppName);
+  inline const static auto SystemSettingPath = QStringLiteral("C:/ProgramData/%2/%3.ini").arg(kAppName, kAppName);
+#elif defined(Q_OS_MAC)
+  inline const static auto UserSettingPath =
+      QStringLiteral("%1/Library/%2/%3.conf").arg(QDir::homePath(), kAppName, kAppName);
+  inline const static auto SystemSettingPath = QStringLiteral("/Libaray/%2/%3.conf").arg(kAppName, kAppName);
+#else
+  inline const static auto UserSettingPath =
+      QStringLiteral("%1/.config/%2/%3.conf").arg(QDir::homePath(), kAppName, kAppName);
+  inline const static auto SystemSettingPath = QStringLiteral("/etc/%2/%3.conf").arg(kAppName, kAppName);
+#endif
+  struct Core
+  {
+    inline static const auto Scope = QStringLiteral("core/loadFromSystemScope");
+  };
+};
+
+class DeskflowSettings : public QObject
+{
+  Q_OBJECT
+public:
+  static DeskflowSettings *instance();
+  static void setValue(const QString &key = QString(), const QVariant &value = QVariant());
+  static QVariant value(const QString &key = QString());
+  static void restoreDefaultSettings();
+  static QVariant defaultValue(const QString &key);
+  static bool isWritable();
+  static bool isSystemScope();
+  static void setScope(bool systemScope);
+
+signals:
+  void scopeChanged(bool isSystemScope);
+  void writableChanged(bool canWrite);
+  void settingsChanged();
+
+private:
+  explicit DeskflowSettings(QObject *parent = nullptr);
+  DeskflowSettings *operator=(DeskflowSettings &other) = delete;
+  DeskflowSettings(const DeskflowSettings &other) = delete;
+  ~DeskflowSettings() = default;
+  void cleanSettings();
+  void initSettings();
+
+  // Vars
+  QSettings *systemSettings = nullptr;
+  QSettings *userSettings = nullptr;
+  QSettings *m_settings = nullptr;
+
+  inline static const QStringList validKeys = {Settings::Core::Scope};
+};
