8b1e8dfd9fe70674302d471f1a03494573d76e0d
sithlord48 <sithlord48@gmail.com>
2025-03-12 22:47:03 -0400

    chore: rm unused CoreTool::getArch

--- src/lib/gui/core/CoreTool.cpp
+++ src/lib/gui/core/CoreTool.cpp
@@ -9,10 +9,8 @@
 
 #if defined(Q_OS_UNIX)
 #include "arch/unix/ArchFileUnix.h"
-#include "arch/unix/ArchSystemUnix.h"
 #elif defined(Q_OS_WIN)
 #include "arch/win32/ArchFileWindows.h"
-#include "arch/win32/ArchSystemWindows.h"
 #endif
 
 #include <QDir>
@@ -35,14 +33,4 @@ QString CoreTool::getInstalledDir() const
   ArchFileWindows sysInfo;
 #endif
   return QDir::cleanPath(QString::fromUtf8(sysInfo.getInstalledDirectory()));
-}
-
-QString CoreTool::getArch() const
-{
-#if defined Q_OS_UNIX
-  ArchSystemUnix sysInfo;
-#elif defined Q_OS_WIN
-  ArchSystemWindows sysInfo;
-#endif
-  return QString::fromUtf8(sysInfo.getPlatformName());
 }
