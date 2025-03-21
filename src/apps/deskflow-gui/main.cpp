e200d52bc9a32d8686047daa6a52de8f0a458cfa
sithlord48 <sithlord48@gmail.com>
2025-03-20 20:40:37 -0400

    refactor: move lib/gui/Diagnostic => apps/deskflow-gui/Diagnostic

--- src/apps/deskflow-gui/main.cpp
+++ src/apps/deskflow-gui/main.cpp
@@ -6,11 +6,12 @@
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
  */
 
+#include "Diagnostic.h"
 #include "MainWindow.h"
 #include "StyleUtils.h"
+
 #include "common/Constants.h"
 #include "common/UrlConstants.h"
-#include "gui/Diagnostic.h"
 #include "gui/DotEnv.h"
 #include "gui/Logger.h"
 #include "gui/Messages.h"
