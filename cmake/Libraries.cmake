455a484125319702c87ed4959044050877c9ea29
sithlord48 <sithlord48@gmail.com>
2025-03-25 21:31:05 -0400

    chore: remove unused define HAVE_LOCALE_H

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -89,7 +89,6 @@ macro(configure_unix_libs)
   include(CheckSymbolExists)
   include(CheckCSourceCompiles)
 
-  check_include_files(locale.h HAVE_LOCALE_H)
   check_include_files(sys/select.h HAVE_SYS_SELECT_H)
   check_include_files(sys/socket.h HAVE_SYS_SOCKET_H)
   check_include_files(sys/time.h HAVE_SYS_TIME_H)
