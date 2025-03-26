56a665cd1821529f2f614a90642b1835700bb60b
sithlord48 <sithlord48@gmail.com>
2025-03-25 21:33:50 -0400

    chore: remove unused define HAVE_SYS_UTSNAME_H

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -92,7 +92,6 @@ macro(configure_unix_libs)
   check_include_files(sys/select.h HAVE_SYS_SELECT_H)
   check_include_files(sys/socket.h HAVE_SYS_SOCKET_H)
   check_include_files(sys/time.h HAVE_SYS_TIME_H)
-  check_include_files(sys/utsname.h HAVE_SYS_UTSNAME_H)
   check_include_files(unistd.h HAVE_UNISTD_H)
   check_include_files(wchar.h HAVE_WCHAR_H)
 
