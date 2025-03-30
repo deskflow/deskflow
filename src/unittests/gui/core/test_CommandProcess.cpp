fdf75e00e0d6b2ef53d339a01a565e4a782974e0
sithlord48 <sithlord48@gmail.com>
2025-03-30 14:35:28 -0400

    refactor: CommandProcessTests to QtTests

--- src/unittests/gui/core/test_CommandProcess.cpp
+++ src/unittests/gui/core/test_CommandProcess.cpp
@@ -1,16 +1,14 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Chris Rizzitello <sithlord48@gmail.com>
  * SPDX-FileCopyrightText: (C) 2024 Symless Ltd.
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
  */
 
-#include "gui/core/CommandProcess.h"
+#include "test_CommandProcess.h"
 
-#include <gtest/gtest.h>
-
-TEST(CommandProcessTests, run_commandSucceeds_returnsOutput)
+void CommandProcess_Test::runCommand()
 {
-
   // it seems that on windows, you can sometimes just call echo (this worked
   // with windows 2022), but on windows 10 you can't call echo directly.
 #if defined(Q_OS_WIN)
@@ -27,5 +25,7 @@ TEST(CommandProcessTests, run_commandSucceeds_returnsOutput)
 
   const QString actual = commandProcess.run();
 
-  EXPECT_EQ("Hello, World!", actual.toStdString());
+  QCOMPARE(actual.toStdString(), "Hello, World!");
 }
+
+QTEST_MAIN(CommandProcess_Test)
