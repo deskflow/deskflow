3cda5d7203c4a58062e819ffce20243ed85ec82d
sithlord48 <sithlord48@gmail.com>
2025-04-05 17:22:14 -0400

    refactor: move ArgParserTests => QtTests

---
+++ src/unittests/deskflow/test_ArgParser.h
@@ -0,0 +1,31 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Chris Rizzitello <sithlord48@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#include "../../lib/deskflow/ArgParser.h"
+#include <QObject>
+
+#include "base/Log.h"
+
+class ArgParser_Test : public QObject
+{
+  Q_OBJECT
+private slots:
+  void initTestCase();
+  // Test are run in order top to bottom
+  void isArg();
+  void missingArg();
+  void withQuotes();
+  void splitCommand();
+  void getArgv();
+  void assembleCommand();
+  void serverArgs();
+  void clientArgs();
+
+private:
+  Arch m_arch;
+  Log m_log;
+  ArgParser m_parser = ArgParser(nullptr);
+};
