fee836a54bd559f6f9f012a4250519e7cb9dc941
sithlord48 <sithlord48@gmail.com>
2025-04-05 20:43:03 -0400

    refactor: port deskflow/ClipboardChunkTests to QtTests

---
+++ src/unittests/deskflow/ClipboardChunksTests.h
@@ -0,0 +1,17 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Chris Rizzitello <sithlord48@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#include <QTest>
+
+class ClipboardChunksTests : public QObject
+{
+  Q_OBJECT
+private slots:
+  // Test are run in order top to bottom
+  void startFormatData();
+  void formatDataChunk();
+  void endFormatData();
+};
