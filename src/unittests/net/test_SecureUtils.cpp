9016c1a897a6ffbafab2cab14ce621f19dfdfc50
sithlord48 <sithlord48@gmail.com>
2025-04-08 18:04:21 -0400

    refactor: move net/SecureUtilsTest => QtTests

--- src/unittests/net/test_SecureUtils.cpp
+++ src/unittests/net/test_SecureUtils.cpp
@@ -5,26 +5,27 @@
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
  */
 
-#include "net/SecureUtils.h"
+#include "test_SecureUtils.h"
+#include "../../lib/net/SecureUtils.h"
 
-#include <gtest/gtest.h>
+using namespace deskflow;
 
-TEST(SecureUtilsTest, formatSSLFingerprints_fromHex_withSeperators)
+void SecureUtils_Test::checkHex()
 {
   std::vector<uint8_t> fingerprint = {40,  253, 10,  152, 138, 14,  161, 108, 215, 232, 108, 167, 238, 88,  65, 113,
                                       202, 178, 142, 73,  37,  148, 144, 37,  38,  5,   141, 175, 99,  237, 46, 48};
 
-  ASSERT_EQ(
+  QCOMPARE(
       deskflow::formatSSLFingerprint(fingerprint, true),
       "28:FD:0A:98:8A:0E:A1:6C:D7:E8:6C:A7:EE:58:41:71:CA:B2:8E:49:25:94:90:25:26:05:8D:AF:63:ED:2E:30"
   );
 }
 
-TEST(SecureUtilsTest, createFingerprintArt)
+void SecureUtils_Test::checkArt()
 {
   std::vector<uint8_t> fingerprint = {40,  253, 10,  152, 138, 14,  161, 108, 215, 232, 108, 167, 238, 88,  65, 113,
                                       202, 178, 142, 73,  37,  148, 144, 37,  38,  5,   141, 175, 99,  237, 46, 48};
-  ASSERT_EQ(
+  QCOMPARE(
       deskflow::generateFingerprintArt(fingerprint), "╔═════════════════╗\n"
                                                      "║*X+. .           ║\n"
                                                      "║*oo +            ║\n"
@@ -38,3 +39,5 @@ TEST(SecureUtilsTest, createFingerprintArt)
                                                      "╚═════════════════╝"
   );
 }
+
+QTEST_MAIN(SecureUtils_Test)
