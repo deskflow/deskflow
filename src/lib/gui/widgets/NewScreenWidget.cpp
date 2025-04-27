a23856ee09dd451e2508b07a737d5589564b4ea8
sithlord48 <sithlord48@gmail.com>
2025-04-27 12:52:48 -0400

    chore: NewScreenWidget, use auto where possible

--- src/lib/gui/widgets/NewScreenWidget.cpp
+++ src/lib/gui/widgets/NewScreenWidget.cpp
@@ -26,10 +26,10 @@ void NewScreenWidget::mousePressEvent(QMouseEvent *event)
   QDataStream dataStream(&itemData, QIODevice::WriteOnly);
   dataStream << -1 << -1 << newScreen;
 
-  QMimeData *pMimeData = new QMimeData;
+  auto *pMimeData = new QMimeData;
   pMimeData->setData(ScreenSetupModel::mimeType(), itemData);
 
-  QDrag *pDrag = new QDrag(this);
+  auto *pDrag = new QDrag(this);
   pDrag->setMimeData(pMimeData);
   pDrag->setPixmap(pixmap());
   pDrag->setHotSpot(event->pos());
