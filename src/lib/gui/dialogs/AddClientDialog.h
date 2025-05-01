6fe549c549043a5ed3dc7f15fe19782de1410858
sithlord48 <sithlord48@gmail.com>
2025-05-01 11:09:41 -0400

    chore: AddClientDialog, add missing nullptr replacement

--- src/lib/gui/dialogs/AddClientDialog.h
+++ src/lib/gui/dialogs/AddClientDialog.h
@@ -29,7 +29,7 @@ class AddClientDialog : public QDialog
 {
   Q_OBJECT
 public:
-  AddClientDialog(const QString &clientName, QWidget *parent = 0);
+  AddClientDialog(const QString &clientName, QWidget *parent = nullptr);
   ~AddClientDialog() override;
 
   int addResult()
