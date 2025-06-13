2772d630a467ceadc6e5ed69102675963c0712ee
sithlord48 <sithlord48@gmail.com>
2025-06-12 20:10:50 -0400

    chore: remove pointless SYS_TIME if defs one is set true and the other never used just include the libs chore: remove TIME_WITH_SYS_TIME set to 1 and used once as include guard

--- src/lib/arch/unix/ArchMultithreadPosix.cpp
+++ src/lib/arch/unix/ArchMultithreadPosix.cpp
@@ -10,18 +10,10 @@
 #include "arch/Arch.h"
 #include "arch/XArch.h"
 
-#include <signal.h>
-#if TIME_WITH_SYS_TIME
-#include <sys/time.h>
-#include <time.h>
-#else
-#if HAVE_SYS_TIME_H
-#include <sys/time.h>
-#else
-#include <time.h>
-#endif
-#endif
 #include <cerrno>
+#include <signal.h>
+#include <sys/time.h>
+#include <time.h>
 
 #define SIGWAKEUP SIGUSR1
 
