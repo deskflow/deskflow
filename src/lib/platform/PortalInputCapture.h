48d6499f246a8ed4444f29338d856b5dc27c9d41
sithlord48 <sithlord48@gmail.com>
2025-06-20 11:20:37 -0400

    refactor: PortalInputCapture, fix a missed non compliant variable names refactor: PortalInputCapture, more const args

--- src/lib/platform/PortalInputCapture.h
+++ src/lib/platform/PortalInputCapture.h
@@ -40,30 +40,34 @@ private:
   void handleSetPointerBarriers(const GObject *object, GAsyncResult *res);
   void handleSessionClosed(XdpSession *session);
   void handleDisabled(const XdpInputCaptureSession *session, const GVariant *option);
-  void handleActivated(const XdpInputCaptureSession *session, std::uint32_t activationId, GVariant *options);
-  void handleDeactivated(const XdpInputCaptureSession *session, std::uint32_t activationId, const GVariant *options);
+  void handleActivated(const XdpInputCaptureSession *session, const std::uint32_t activationId, GVariant *options);
+  void
+  handleDeactivated(const XdpInputCaptureSession *session, const std::uint32_t activationId, const GVariant *options);
   void handleZonesChanged(XdpInputCaptureSession *session, const GVariant *options);
 
   /// g_signal_connect callback wrapper
-  static void sessionClosed(XdpSession *session, gpointer data)
+  static void sessionClosed(XdpSession *session, const gpointer data)
   {
     static_cast<PortalInputCapture *>(data)->handleSessionClosed(session);
   }
-  static void disabled(XdpInputCaptureSession *session, GVariant *options, gpointer data)
+  static void disabled(const XdpInputCaptureSession *session, const GVariant *options, const gpointer data)
   {
     static_cast<PortalInputCapture *>(data)->handleDisabled(session, options);
   }
-  static void
-  activated(const XdpInputCaptureSession *session, std::uint32_t activationId, GVariant *options, gpointer data)
+  static void activated(
+      const XdpInputCaptureSession *session, const std::uint32_t activationId, GVariant *options, const gpointer data
+  )
   {
     static_cast<PortalInputCapture *>(data)->handleActivated(session, activationId, options);
   }
-  static void
-  deactivated(const XdpInputCaptureSession *session, std::uint32_t activationId, const GVariant *options, gpointer data)
+  static void deactivated(
+      const XdpInputCaptureSession *session, const std::uint32_t activationId, const GVariant *options,
+      const gpointer data
+  )
   {
     static_cast<PortalInputCapture *>(data)->handleDeactivated(session, activationId, options);
   }
-  static void zonesChanged(XdpInputCaptureSession *session, GVariant *options, gpointer data)
+  static void zonesChanged(XdpInputCaptureSession *session, const GVariant *options, const gpointer data)
   {
     static_cast<PortalInputCapture *>(data)->handleZonesChanged(session, options);
   }
@@ -89,7 +93,7 @@ private:
   XdpPortal *m_portal = nullptr;
   XdpInputCaptureSession *m_session = nullptr;
 
-  std::map<Signal, guint> m_signals = {
+  std::map<Signal, gulong> m_signals = {
       {Signal::SessionClosed, 0},
       {Signal::Disabled, 0},
       {Signal::Activated, 0},
