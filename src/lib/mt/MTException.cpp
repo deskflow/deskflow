22a358fb49f58fbb4114075b0ed23aafbc59a835
sithlord48 <sithlord48@gmail.com>
2025-08-07 16:57:23 -0400

    refactor: XMT Classes => MTException Classes

--- src/lib/mt/MTException.cpp
+++ src/lib/mt/MTException.cpp
@@ -1,17 +1,18 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Developers
  * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
  * SPDX-FileCopyrightText: (C) 2002 Chris Schoeneman
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
  */
 
-#include "XMT.h"
+#include "MTException.h"
 
 //
-// XMTThreadUnavailable
+// MTThreadUnavailableException
 //
 
-std::string XMTThreadUnavailable::getWhat() const throw()
+std::string MTThreadUnavailableException::getWhat() const throw()
 {
-  return format("XMTThreadUnavailable", "cannot create thread");
+  return format("MTThreadUnavailableException", "cannot create thread");
 }
