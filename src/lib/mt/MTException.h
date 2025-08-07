93e98095480913f258dd8be444d7b823f35478f5
sithlord48 <sithlord48@gmail.com>
2025-08-07 16:57:23 -0400

    refactor: XMT Classes => MTException Classes

--- src/lib/mt/MTException.h
+++ src/lib/mt/MTException.h
@@ -1,5 +1,6 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Developers
  * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
  * SPDX-FileCopyrightText: (C) 2002 Chris Schoeneman
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
@@ -10,19 +11,19 @@
 #include "base/XBase.h"
 
 /**
- * @brief XMT generic multithreading exception
+ * @brief MTException generic multithreading exception
  */
-class XMT : public XBase
+class MTException : public XBase
 {
   using XBase::XBase;
 };
 
 /**
- * @brief XMTThreadUnavailable - Thrown when a thread cannot be created.
+ * @brief MTThreadUnavailableException - Thrown when a thread cannot be created.
  */
-class XMTThreadUnavailable : public XMT
+class MTThreadUnavailableException : public MTException
 {
-  using XMT::XMT;
+  using MTException::MTException;
 
 protected:
   std::string getWhat() const throw() override;
