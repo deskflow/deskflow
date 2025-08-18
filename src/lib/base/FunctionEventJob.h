aae840d0ed016e7010c3eb7844933253a14fb877
sithlord48 <sithlord48@gmail.com>
2025-08-17 22:53:30 -0400

    refactor: remove IEventJob

--- src/lib/base/FunctionEventJob.h
+++ src/lib/base/FunctionEventJob.h
@@ -1,5 +1,6 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Developers
  * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
  * SPDX-FileCopyrightText: (C) 2004 Chris Schoeneman
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
@@ -7,21 +8,19 @@
 
 #pragma once
 
-#include "base/IEventJob.h"
+class Event;
 
 //! Use a function as an event job
 /*!
 An event job class that invokes a function.
 */
-class FunctionEventJob : public IEventJob
+class FunctionEventJob
 {
 public:
   //! run() invokes \c func(arg)
   explicit FunctionEventJob(void (*func)(const Event &, void *), void *arg = nullptr);
-  ~FunctionEventJob() override = default;
-
-  // IEventJob overrides
-  void run(const Event &) override;
+  ~FunctionEventJob() = default;
+  void run(const Event &);
 
 private:
   void (*m_func)(const Event &, void *);
