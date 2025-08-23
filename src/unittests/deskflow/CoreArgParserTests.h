57bec86134808df27bf9f3f5dcbe0bbe63d7f649
sithlord48 <sithlord48@gmail.com>
2025-08-23 16:10:13 -0400

    refactor: move preventsleep option into CoreArgs

--- src/unittests/deskflow/CoreArgParserTests.h
+++ src/unittests/deskflow/CoreArgParserTests.h
@@ -25,6 +25,10 @@ private Q_SLOTS:
   void logFileWithSpace();
   void insecure();
   void tlsCert();
+  void preventSleep_true();
+  void preventSleep_false();
+  void preventSleep_1();
+  void preventSleep_0();
 
 private:
   inline static const QString m_settingsPath = QStringLiteral("tmp/test");
