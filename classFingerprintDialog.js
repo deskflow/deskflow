e2e2d4269db7f301e592f9402a63c2302f9fbeaf
sithlord48 <sithlord48@gmail.com>
2025-08-27 23:25:09 +0000

    Deploying to gh-pages from @ deskflow/deskflow@e7880b72f1fe5c272c296938a3d174308b9864c7 🚀

--- classFingerprintDialog.js
+++ classFingerprintDialog.js
@@ -1,6 +1,5 @@
 var classFingerprintDialog =
 [
-    [ "FingerprintDialog", "classFingerprintDialog.html#a5b422dcd4f32bd0a4d84674d137627af", null ],
-    [ "~FingerprintDialog", "classFingerprintDialog.html#adcf7b7eadc6efe8103cadd81968f0f6f", null ],
-    [ "requestLocalPrintsDialog", "classFingerprintDialog.html#af1d99de0eecf9ef078a434f665dfb4f1", null ]
+    [ "FingerprintDialog", "classFingerprintDialog.html#a01b60a7f7716d842bd268d400e5f1c78", null ],
+    [ "~FingerprintDialog", "classFingerprintDialog.html#adcf7b7eadc6efe8103cadd81968f0f6f", null ]
 ];
--- classFingerprintPreview.js
+++ classFingerprintPreview.js
@@ -1,5 +1,6 @@
 var classFingerprintPreview =
 [
-    [ "FingerprintPreview", "classFingerprintPreview.html#a74f0f523577a151c942a450854e228ee", null ],
-    [ "~FingerprintPreview", "classFingerprintPreview.html#a3d31e6ecd6e5a85c20201f13d7f35fff", null ]
+    [ "FingerprintPreview", "classFingerprintPreview.html#a89faeaec9798380efefae29e9e5e26aa", null ],
+    [ "~FingerprintPreview", "classFingerprintPreview.html#a3d31e6ecd6e5a85c20201f13d7f35fff", null ],
+    [ "toggleMode", "classFingerprintPreview.html#ac5d86965ea349122495cefbe634108ab", null ]
 ];
