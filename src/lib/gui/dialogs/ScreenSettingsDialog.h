360c647906fad8dbd1ba871cb85070c518366a40
sithlord48 <sithlord48@gmail.com>
2025-09-01 18:59:14 -0400

    refactor: ScreenSettingsDialog, conform to naming conventions

--- src/lib/gui/dialogs/ScreenSettingsDialog.h
+++ src/lib/gui/dialogs/ScreenSettingsDialog.h
@@ -24,19 +24,19 @@ class ScreenSettingsDialog : public QDialog
   Q_OBJECT
 
 public:
-  explicit ScreenSettingsDialog(QWidget *parent, Screen *pScreen = nullptr, const ScreenList *pScreens = nullptr);
+  explicit ScreenSettingsDialog(QWidget *parent, Screen *screen = nullptr, const ScreenList *screens = nullptr);
   ~ScreenSettingsDialog() override;
 
 public Q_SLOTS:
   void accept() override;
 
 private Q_SLOTS:
-  void on_m_pButtonAddAlias_clicked();
-  void on_m_pButtonRemoveAlias_clicked();
-  void on_m_pLineEditAlias_textChanged(const QString &text);
-  void on_m_pListAliases_itemSelectionChanged();
+  void on_btnAddAlias_clicked();
+  void on_btnRemoveAlias_clicked();
+  void on_lineAddAlias_textChanged(const QString &text);
+  void on_listAliases_itemSelectionChanged();
 
 private:
   std::unique_ptr<Ui::ScreenSettingsDialog> ui;
-  Screen *m_pScreen;
+  Screen *m_screen;
 };
