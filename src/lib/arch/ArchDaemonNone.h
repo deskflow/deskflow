0a7d311a7e7a6b32e6dc9733f85727dad4d5e87c
sithlord48 <sithlord48@gmail.com>
2025-10-15 18:05:16 -0400

    chore: remove Cpp file for ArchDaemonNone

--- src/lib/arch/ArchDaemonNone.h
+++ src/lib/arch/ArchDaemonNone.h
@@ -1,5 +1,6 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Developers.
  * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
  * SPDX-FileCopyrightText: (C) 2002 Chris Schoeneman
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
@@ -24,6 +25,14 @@ public:
   ~ArchDaemonNone() override = default;
 
   // IArchDaemon overrides
-  int daemonize(DaemonFunc const &func) override;
-  QString commandLine() const override;
+  int daemonize(DaemonFunc const &func) override
+  {
+    // simply forward the call to func.  obviously, this doesn't
+    // do any daemonizing.
+    return func();
+  }
+  QString commandLine() const override
+  {
+    return {};
+  }
 };
