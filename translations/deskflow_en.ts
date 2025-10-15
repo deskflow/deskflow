dc69f250c65fbf2e509e74d8ddf47c90282cd468
sithlord48 <sithlord48@gmail.com>
2025-10-14 22:39:45 -0400

    feat: support translation generation

---
+++ translations/deskflow_en.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message numerus="yes">
+        <source>%1 is connected, with %n client(s): %2</source>
+        <extracomment>Shown when in server mode and at least 1 client is connected %1 is replaced by the app name %2 will be a list of at least one client %n will be replaced by the number of clients (n is &gt;=1), it is not requried to be in the translation</extracomment>
+        <translation>
+            <numerusform>%1 is connected, with a client: %2</numerusform>
+            <numerusform>%1 is connected, with %n clients: %2</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
