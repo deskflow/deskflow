d65ac151f0a29af232eec3c8b3c250803b7e7cfa
sithlord48 <sithlord48@gmail.com>
2025-10-27 20:36:01 -0400

    refactor: deskflow-gui add newline after the lang is written to console

--- src/apps/deskflow-gui/deskflow-gui.cpp
+++ src/apps/deskflow-gui/deskflow-gui.cpp
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
 
   // Ensure the I18N object is made before strings
-  QTextStream(stdout) << "initial language: " << I18N::currentLanguage();
+  QTextStream(stdout) << "initial language: " << I18N::currentLanguage() << '\n';
 
   // Add Command Line Options
   auto helpOption = QCommandLineOption({"h", "help"}, "Display Help on the command line");
