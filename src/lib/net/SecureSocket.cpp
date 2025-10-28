83e0a6b1ea33da03bdcd7de136f493e40630ca7f
sithlord48 <sithlord48@gmail.com>
2025-10-28 16:28:18 -0400

    refactor: SSL_Options, ignore client disconnect w/o close message

--- src/lib/net/SecureSocket.cpp
+++ src/lib/net/SecureSocket.cpp
@@ -357,7 +357,10 @@ void SecureSocket::initContext(bool server)
 
   // Prevent the usage of of all version prior to TLSv1.2 as they are known to
   // be vulnerable
-  SSL_CTX_set_options(m_ssl->m_context, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_TLSv1 | SSL_OP_NO_TLSv1_1);
+  SSL_CTX_set_options(
+      m_ssl->m_context,
+      SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_TLSv1 | SSL_OP_NO_TLSv1_1 | SSL_OP_IGNORE_UNEXPECTED_EOF
+  );
 
   if (m_ssl->m_context == nullptr) {
     SslLogger::logError();
