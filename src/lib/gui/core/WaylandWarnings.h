7317d20b487fc9c78bbe0caa190b6eed3dfd0ebe
sithlord48 <sithlord48@gmail.com>
2025-11-05 15:45:56 -0500

    refactor: platform/wayland move consts kHasEI etc into wayland Warnings

--- src/lib/gui/core/WaylandWarnings.h
+++ src/lib/gui/core/WaylandWarnings.h
@@ -1,5 +1,6 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Chris Rizzitello <sithlord48@gmail.com>
  * SPDX-FileCopyrightText: (C) 2024 Symless Ltd.
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
  */
@@ -8,8 +9,6 @@
 
 #include <QWidget>
 
-#include "platform/Wayland.h"
-
 namespace deskflow::gui::core {
 
 class WaylandWarnings
@@ -17,13 +16,28 @@ class WaylandWarnings
 public:
   explicit WaylandWarnings() = default;
 
-  void showOnce(
-      QWidget *parent, bool hasEi = platform::kHasEi, bool hasPortal = platform::kHasPortal,
-      bool hasPortalInputCapture = platform::kHasPortalInputCapture
-  );
+  void showOnce(QWidget *parent);
 
 private:
   bool m_errorShown{false};
+
+#if WINAPI_LIBEI
+  const bool m_hasEi = true;
+#else
+  const bool m_hasEi = false;
+#endif
+
+#if WINAPI_LIBPORTAL
+  const bool m_hasPortal = true;
+#else
+  const bool m_hasPortal = false;
+#endif
+
+#if HAVE_LIBPORTAL_INPUTCAPTURE
+  const bool m_hasPortalInputCapture = true;
+#else
+  const bool m_hasPortalInputCapture = false;
+#endif
 };
 
 } // namespace deskflow::gui::core
