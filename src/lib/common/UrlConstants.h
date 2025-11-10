bbd306c4c80d7cd45f193793e4039503230e1448
sithlord48 <sithlord48@gmail.com>
2025-11-10 13:11:07 -0500

    refactor UrlConstants use Qt::StringLiterals namespace

--- src/lib/common/UrlConstants.h
+++ src/lib/common/UrlConstants.h
@@ -8,18 +8,19 @@
 
 #include <QString>
 
+using namespace Qt::StringLiterals;
 // important: this is used for settings paths on some platforms,
 // and must not be a url. qt automatically converts this to reverse domain
 // notation (rdn), e.g. org.deskflow
-const auto kOrgDomain = QStringLiteral("deskflow.org");
+const auto kOrgDomain = u"deskflow.org"_s;
 
-const auto kUrlSourceQuery = QStringLiteral("source=gui");
-const auto kUrlApp = QStringLiteral("https://%1").arg(kOrgDomain);
-const auto kUrlHelp = QStringLiteral("%1/help?%2").arg(kUrlApp, kUrlSourceQuery);
-const auto kUrlDownload = QStringLiteral("%1/download?%2").arg(kUrlApp, kUrlSourceQuery);
+const auto kUrlSourceQuery = u"source=gui"_s;
+const auto kUrlApp = u"https://%1"_s.arg(kOrgDomain);
+const auto kUrlHelp = u"%1/help?%2"_s.arg(kUrlApp, kUrlSourceQuery);
+const auto kUrlDownload = u"%1/download?%2"_s.arg(kUrlApp, kUrlSourceQuery);
 
-const auto kUrlUpdateCheck = QStringLiteral("https://api.%1/version").arg(kOrgDomain);
+const auto kUrlUpdateCheck = u"https://api.%1/version"_s.arg(kOrgDomain);
 
 #if defined(Q_OS_LINUX)
-const auto kUrlGnomeTrayFix = QStringLiteral("https://extensions.gnome.org/extension/615/appindicator-support/");
+const auto kUrlGnomeTrayFix = u"https://extensions.gnome.org/extension/615/appindicator-support/"_s;
 #endif
