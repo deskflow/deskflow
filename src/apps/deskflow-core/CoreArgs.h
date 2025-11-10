859af720c9d7432282e0f292d7dfa814c3ec28f3
sithlord48 <sithlord48@gmail.com>
2025-11-10 16:40:21 -0500

    feat: add a new core option `--new-instance` to skip the core check for a running instance

--- src/apps/deskflow-core/CoreArgs.h
+++ src/apps/deskflow-core/CoreArgs.h
@@ -15,8 +15,10 @@ struct CoreArgs
 {
   inline static const auto helpOption = QCommandLineOption({"h", "help"}, "Display Help on the command line");
   inline static const auto versionOption = QCommandLineOption({"v", "version"}, "Display version information");
+  inline static const auto multiInstanceOption =
+      QCommandLineOption("new-instance", "Skip the check for a running instance, always makes a new instance");
   inline static const auto configOption =
       QCommandLineOption({"s", "settings"}, "override configuration file to use", "configFile");
 
-  inline static const auto options = {helpOption, versionOption, configOption};
+  inline static const auto options = {helpOption, versionOption, multiInstanceOption, configOption};
 };
