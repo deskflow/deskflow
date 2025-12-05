2d682d9151892298c7fc4aec8f6e10872277111c
sithlord48 <sithlord48@gmail.com>
2025-12-05 16:30:07 -0500

    chore: io/FileSystem remove unused methods

--- src/lib/io/Filesystem.h
+++ src/lib/io/Filesystem.h
@@ -9,18 +9,10 @@
 
 #include <cstdio>
 #include <filesystem>
-#include <ios>
-#include <iosfwd>
 
 namespace deskflow {
 
 namespace fs = std::filesystem;
-
-void openUtf8Path(std::ifstream &stream, const fs::path &path, std::ios_base::openmode mode = std::ios_base::in);
-void openUtf8Path(std::ofstream &stream, const fs::path &path, std::ios_base::openmode mode = std::ios_base::out);
-void openUtf8Path(
-    std::fstream &stream, const fs::path &path, std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out
-);
 
 std::FILE *fopenUtf8Path(const fs::path &path, const std::string &mode);
 
