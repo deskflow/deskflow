abeb0da4a1b2a714d8e04ce2907825b023415f20
Nick Bolton <nick@symless.com>
2025-12-05 17:45:09 +0000

    wip: Create Core IPC classes

---
+++ src/lib/deskflow/ipc/CoreIpcServer.h
@@ -0,0 +1,30 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Symless Ltd.
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#pragma once
+
+#include "IpcServer.h"
+
+#include <QObject>
+#include <QSet>
+
+class QLocalServer;
+class QLocalSocket;
+
+namespace deskflow::core::ipc {
+
+class CoreIpcServer : public IpcServer
+{
+  Q_OBJECT
+
+public:
+  explicit CoreIpcServer(QObject *parent);
+
+private:
+  void processMessage(QLocalSocket *clientSocket, const QString &message) override;
+};
+
+} // namespace deskflow::core::ipc
