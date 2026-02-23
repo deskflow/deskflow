67a68d8b0409ef2f854487a2ecef2cfe4d950f94
nbolton <nick@symless.com>
2026-02-23 21:11:13 +0000

    Deploying to gh-pages from @ deskflow/deskflow@033e5530c3549dbff348561c9f8e70fa0f933b56 🚀

--- structSettings_1_1Gui.js
+++ structSettings_1_1Gui.js
@@ -6,7 +6,6 @@ var structSettings_1_1Gui =
     [ "CloseReminder", "structSettings_1_1Gui.html#aa38501d12ea2f102a9ff82a722c55d08", null ],
     [ "CloseToTray", "structSettings_1_1Gui.html#ad13866ec8710e35b73149cd35baae69d", null ],
     [ "LogExpanded", "structSettings_1_1Gui.html#af34e29ae4ea0d59206dbe9094321652f", null ],
-    [ "ShowGenericClientFailureDialog", "structSettings_1_1Gui.html#a9bdc01411a38ecff09aab7d22056ce8d", null ],
     [ "ShownFirstConnectedMessage", "structSettings_1_1Gui.html#aabe88e8a490d567b3c0aadad86d8c33a", null ],
     [ "ShownServerFirstStartMessage", "structSettings_1_1Gui.html#adf087c1172fcbfa7bca804d992bfff6a", null ],
     [ "ShowVersionInTitle", "structSettings_1_1Gui.html#acf965f4f970c6d319f7d349816518b95", null ],
