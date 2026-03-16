d52f3e6fd391a832d3fb633705af7beb8cd26c44
nbolton <nick@symless.com>
2026-03-16 12:40:54 +0000

    Deploying to gh-pages from @ deskflow/deskflow@23c8496e249e9271a65d593fe869706aea35f55e 🚀

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['elevate_0',['Elevate',['../structSettings_1_1Daemon.html#a771cef30da81388437b4565ce8c3ec21',1,'Settings::Daemon']]],
-  ['error_1',['Error',['../structIArchNetwork_1_1PollEventMask.html#a63c05ada1c4ff3608177a5df753753e2',1,'IArchNetwork::PollEventMask']]],
-  ['exceptfds_2',['exceptfds',['../ArchNetworkWinsock_8cpp.html#a83b39b991fea62ae3743fc34f1c49331',1,'ArchNetworkWinsock.cpp']]],
-  ['externalconfig_3',['ExternalConfig',['../structSettings_1_1Server.html#ae54c06a2036b14062071d2ea36fb09e2',1,'Settings::Server']]],
-  ['externalconfigfile_4',['ExternalConfigFile',['../structSettings_1_1Server.html#a25a7367e2f17cf0864220cc913d5245d',1,'Settings::Server']]]
+  ['enableentercommand_1',['EnableEnterCommand',['../structSettings_1_1Core.html#a749b9ead3cf51e4685956adabbb17b67',1,'Settings::Core']]],
+  ['enableexitcommand_2',['EnableExitCommand',['../structSettings_1_1Core.html#a495a72312979325c8c2528f044668f02',1,'Settings::Core']]],
+  ['error_3',['Error',['../structIArchNetwork_1_1PollEventMask.html#a63c05ada1c4ff3608177a5df753753e2',1,'IArchNetwork::PollEventMask']]],
+  ['exceptfds_4',['exceptfds',['../ArchNetworkWinsock_8cpp.html#a83b39b991fea62ae3743fc34f1c49331',1,'ArchNetworkWinsock.cpp']]],
+  ['externalconfig_5',['ExternalConfig',['../structSettings_1_1Server.html#ae54c06a2036b14062071d2ea36fb09e2',1,'Settings::Server']]],
+  ['externalconfigfile_6',['ExternalConfigFile',['../structSettings_1_1Server.html#a25a7367e2f17cf0864220cc913d5245d',1,'Settings::Server']]]
 ];
