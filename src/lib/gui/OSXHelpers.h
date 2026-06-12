c6a393c82c264bdf8e5300ae914a959b062b140e
Ashley Stonham <ashley@stonham.com.au>
2026-06-12 10:29:09 +1000

    fix(gui): avoid QSystemTrayIcon crash on macOS 27
    fixes: #9835

--- src/lib/gui/OSXHelpers.h
+++ src/lib/gui/OSXHelpers.h
@@ -1,5 +1,6 @@
 /*
  * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2026 Deskflow Developers
  * SPDX-FileCopyrightText: (C) 2015 Synergy App Ltd
  * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
  */
@@ -8,9 +9,15 @@
 
 #include <QString>
 
+class QIcon;
+class QMenu;
+
 void requestOSXNotificationPermission();
 bool isOSXDevelopmentBuild();
 bool showOSXNotification(const QString &title, const QString &body);
 bool isOSXInterfaceStyleDark();
 void forceAppActive();
 void macOSNativeHide();
+void setupMacOSStatusItem(QMenu *menu);
+void setMacOSStatusItemIcon(const QIcon &icon);
+void cleanupMacOSStatusItem();
