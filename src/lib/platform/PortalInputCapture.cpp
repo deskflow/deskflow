0f2f2d742d848a679df126b9c916447421d385e9
sithlord48 <sithlord48@gmail.com>
2026-06-18 19:36:37 -0400

    reactor: mark that session var maybe unused

--- src/lib/platform/PortalInputCapture.cpp
+++ src/lib/platform/PortalInputCapture.cpp
@@ -247,7 +247,7 @@ void PortalInputCapture::handleSessionClosed(XdpSession *session)
   m_signals.at(Signal::SessionClosed) = 0;
 }
 
-void PortalInputCapture::claimClipboardOwnership(XdpSession *session)
+void PortalInputCapture::claimClipboardOwnership([[maybe_unused]] XdpSession *session)
 {
 #ifdef HAVE_LIBPORTAL_CLIPBOARD
   PortalClipboard::claimOwnership(m_clipboard, session);
