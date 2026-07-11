95f99b0b6fe950384ff82ba7ccd0824b8a38dd69
sithlord48 <sithlord48@gmail.com>
2026-07-10 20:11:21 -0400

    refactor(settings): add new method to return known screen names

--- src/lib/common/Settings.h
+++ src/lib/common/Settings.h
@@ -201,6 +201,7 @@ public:
   static QString portableSettingsFile();
   static void removeUnknownScreens(const QStringList &knownScreens);
   static QVariant screenDefaults(const QString &key);
+  static QStringList knownScreens();
 
 Q_SIGNALS:
   void settingsChanged(const QString key);
